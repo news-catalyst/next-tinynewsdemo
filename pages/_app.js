8734cbce321b1d8e8dc2da327b3d9369e4a50e2b
Tyler Fisher <tyler@tylerjfisher.com>
2020-07-08 16:37:25 -0400

    Google Analytics integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
-import '../styles.scss'
+import '../styles.scss';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import * as gtag from '../lib/gtag';
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
+
+export default App;
