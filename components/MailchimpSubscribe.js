e647bdd60b8147e62f0097927e4cb50675d8b937
Tyler Fisher <tyler@tylerjfisher.com>
2020-07-08 15:25:48 -0400

    Make mailchimp URL an environment variable

--- components/MailchimpSubscribe.js
+++ components/MailchimpSubscribe.js
@@ -1,7 +1,7 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 // you can get this url from the embed code form action
-const url = "https://tinynewsco.us10.list-manage.com/subscribe/post?u=affe71ae108c8047b9e1e68c8&amp;id=fb3e2377f7"
+const url = process.env.NEXT_PUBLIC_MAILCHIMP_SUBSCRIBE_URL;
 
 const Newsletter = () => <MailchimpSubscribe url={url} />
 
