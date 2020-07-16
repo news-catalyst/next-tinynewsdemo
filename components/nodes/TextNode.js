55f722ffe5673ae12806379602061725ccaae377
Tyler Fisher <tyler@tylerjfisher.com>
2020-07-16 13:17:22 -0400

    following the example

--- components/nodes/TextNode.js
+++ components/nodes/TextNode.js
@@ -1,15 +1,19 @@
 export default function TextNode({ node }) {
   const processChild = function(child) {
     let text = (<span>{child.content} </span>);
-    if (child.style.underline) {
-      text = (<u>{text}</u>);
+
+    if (child.style) {
+      if (child.style.underline) {
+        text = (<u>{text}</u>);
+      }
+      if (child.style.italic) {
+        text = (<em>{text}</em>);
+      }
+      if (child.style.bold) {
+        text = (<strong>{text}</strong>);
+      }
     }
-    if (child.style.italic) {
-      text = (<em>{text}</em>);
-    }
-    if (child.style.bold) {
-      text = (<strong>{text}</strong>);
-    }
+
     if (child.link) {
       text = (<a href={child.link}>{text}</a>);
     }
