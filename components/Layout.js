38173cb9c285269a1646aca8720051e66f36ed56
Tyler Fisher <tyler@tylerjfisher.com>
2020-07-17 14:56:33 -0400

    AMP analytics working

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { siteMetadata } from "../lib/siteMetadata.js";
 import globalStyles from '../styles/global.js'
+import { useAmp } from 'next/amp';
+import AmpAnalytics from './amp/AmpAnalytics.js';
 
 export default function Layout({ children, meta }) {
   const metaValues = {
@@ -12,6 +14,10 @@ export default function Layout({ children, meta }) {
     'canonical': meta.canonical || siteMetadata.siteUrl,
   };
 
+  const isAmp = useAmp();
+
+  const trackingId = process.env.GA_TRACKING_ID;
+
   return (
     <>
       <Head>
@@ -22,8 +28,36 @@ export default function Layout({ children, meta }) {
         <meta property="og:description" content={metaValues.ogDescription} />
         <meta property="og:image" content={metaValues.ogImage} />
         <link rel="canonical" href={metaValues.canonical} />
+        {isAmp && (
+          <script
+            async
+            custom-element="amp-analytics"
+            src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"
+            key="amp-analytics"
+           />
+        )}
       </Head>
       <main className="container">
+        {isAmp && (
+          <AmpAnalytics
+            type="googleanalytics"
+            script={{
+              vars: {
+                account: trackingId,
+                gtag_id: trackingId,
+                config: {
+                  [trackingId]: { groups: 'default' },
+                },
+              },
+              triggers: {
+                trackPageview: {
+                  on: 'visible',
+                  request: 'pageview',
+                },
+              },
+            }}
+          />
+        )}
         {children}
       </main>
       <style jsx global>
