e0b4f4b560b27a8e493a1f0842624cb54e1c0083
Tyler Fisher <tyler@tylerjfisher.com>
2020-07-17 15:11:19 -0400

    Use Google fonts correctly

--- components/Layout.js
+++ components/Layout.js
@@ -28,6 +28,7 @@ export default function Layout({ children, meta }) {
         <meta property="og:description" content={metaValues.ogDescription} />
         <meta property="og:image" content={metaValues.ogImage} />
         <link rel="canonical" href={metaValues.canonical} />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Libre+Franklin:ital,wght@0,300;0,800;1,300;1,800&display=swap" rel="stylesheet" />
         {isAmp && (
           <script
             async
