702b62c8d5796d52c0c022ef6eb79b139dc064b2
Jacqui Lough <lough.jacqui@gmail.com>
2020-07-29 08:59:41 +1000

    Style sub-list items up to two indent levels

--- components/nodes/ListNode.js
+++ components/nodes/ListNode.js
@@ -1,25 +1,45 @@
+// import ListItemNode from './ListItemNode.js';
 import TextNode from './TextNode.js';
 
 export default function ListNode({ node }) {
+  let items = [];
+  node.items.map(function (item) {
+    if (item.nestingLevel === 1) {
+      items.push(
+        <li key={item.index} style={{ listStyle: 'none' }}>
+          <ul>
+            <li>
+              <TextNode node={item} />
+            </li>
+          </ul>
+        </li>
+      );
+    } else if (item.nestingLevel === 2) {
+      items.push(
+        <li key={item.index} style={{ listStyle: 'none' }}>
+          <ul>
+            <li style={{ listStyle: 'none' }}>
+              <ul>
+                <li>
+                  <TextNode node={item} />
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      );
+    } else {
+      items.push(
+        <li key={item.index}>
+          <TextNode node={item} />
+        </li>
+      );
+    }
+  });
+
   if (node.listType === 'NUMBER') {
-    return (
-      <ol>
-        {node.items.map((item) => (
-          <li key={item.index}>
-            <TextNode node={item} />
-          </li>
-        ))}
-      </ol>
-    );
+    return <ol>{items}</ol>;
   } else {
-    return (
-      <ul>
-        {node.items.map((item) => (
-          <li key={item.index}>
-            <TextNode node={item} />
-          </li>
-        ))}
-      </ul>
-    );
+    return <ul>{items}</ul>;
   }
 }
