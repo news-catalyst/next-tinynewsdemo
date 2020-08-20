8db888dfa428e3f81546323092970b028b499577
Jacqui Lough <lough.jacqui@gmail.com>
2020-08-21 08:59:25 +1000

    dynamic homepage step 1: find selected layout, data; dynamically render the correct component

---
+++ components/homepage/LargePackageStoryLead.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function LargePackageStoryLead(props) {
+  console.log('LargePackageStoryLead', props);
+  return (
+    <div>
+      <h1>LargePackageStoryLead</h1>
+    </div>
+  );
+}
