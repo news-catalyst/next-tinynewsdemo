52dcfdea80e20bdb64eb326e49e18e2176ae145d
Jacqui Lough <lough.jacqui@gmail.com>
2020-08-24 11:39:18 +1000

    dynamic homepage step 3: render homepage for LargePackageStory

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
 import _ from 'lodash';
-import Link from 'next/link';
+import dynamic from 'next/dynamic';
 import { getHomepageData } from '../lib/homepage.js';
 import {
-  getArticleBySlug,
-  listAllArticles,
   listAllTags,
   listAllSections,
   getHomepageArticles,
 } from '../lib/articles.js';
-import Layout from '../components/Layout.js';
-import ArticleNav from '../components/nav/ArticleNav.js';
-import FeaturedArticleLink from '../components/homepage/FeaturedArticleLink.js';
-import ArticleLink from '../components/homepage/ArticleLink.js';
-import ArticleFooter from '../components/nav/ArticleFooter.js';
-// import HomepageSearchPanel from '../components/homepage/HomepageSearchPanel.js'
-import { useAmp } from 'next/amp';
-import { siteMetadata } from '../lib/siteMetadata.js';
-
-import dynamic from 'next/dynamic';
 
 const BigFeaturedStory = dynamic(() =>
   import(`../components/homepage/BigFeaturedStory`)
@@ -26,12 +14,7 @@ const LargePackageStoryLead = dynamic(() =>
   import(`../components/homepage/LargePackageStoryLead`)
 );
 
-export const config = { amp: 'hybrid' };
-
-export default function Home({ hpData, hpArticles, articles, tags, sections }) {
-  const isAmp = useAmp();
-
-  console.log('hpdata:', hpData);
+export default function Home({ hpData, hpArticles, tags, sections }) {
   return (
     <>
       {hpData.layoutComponent === 'BigFeaturedStory' && (
@@ -42,82 +25,23 @@ export default function Home({ hpData, hpArticles, articles, tags, sections }) {
         />
       )}
       {hpData.layoutComponent === 'LargePackageStoryLead' && (
-        <LargePackageStoryLead articles={hpData.articles} />
+        <LargePackageStoryLead
+          articles={hpArticles}
+          tags={tags}
+          sections={sections}
+        />
       )}
     </>
-  );
-
-  // let hpComponent = `<${hpData.layoutComponent} articles=${hpData.articles} />`;
-  // console.log("HP component: ", hpComponent);
-
-  siteMetadata.tags = tags;
-
-  let featuredArticles = articles.slice(0, 1);
-  let unfeaturedArticles = articles.slice(1);
-
-  const tagLinks = tags.map((tag) => (
-    <Link key={tag.title} href={`/tags/${tag.title}`}>
-      <a className="panel-block is-active">{_.startCase(tag.title)}</a>
-    </Link>
-  ));
-
-  return (
-    <div className="homepage">
-      <ArticleNav metadata={siteMetadata} tags={tags} sections={sections} />
-      <Layout meta={siteMetadata}>
-        <section className="hero is-dark is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">{siteMetadata.homepageTitle}</h1>
-              <h2 className="subtitle">{siteMetadata.homepageSubtitle}</h2>
-            </div>
-          </div>
-        </section>
-        <div className="featured-article">
-          {featuredArticles.map((featuredArticle) => (
-            <FeaturedArticleLink
-              key={featuredArticle.id}
-              article={featuredArticle}
-              amp={isAmp}
-            />
-          ))}
-        </div>
-        <section className="section">
-          <div className="columns">
-            <div className="column is-four-fifths">
-              {unfeaturedArticles.map((unfeaturedArticle) => (
-                <ArticleLink
-                  key={unfeaturedArticle.id}
-                  article={unfeaturedArticle}
-                  amp={isAmp}
-                />
-              ))}
-            </div>
-            <div className="column">
-              {/* <HomepageSearchPanel metadata={siteMetadata} query={query} setQuery={setQuery} /> */}
-              <nav className="panel">
-                <p className="panel-heading">{siteMetadata.labels.topics}</p>
-                {tagLinks}
-              </nav>
-            </div>
-          </div>
-        </section>
-      </Layout>
-      <ArticleFooter post_type="home" metadata={siteMetadata} />
-    </div>
   );
 }
 
 export async function getStaticProps() {
-  // to-do:
   //    get selected homepage layout / data
   const hpData = await getHomepageData();
   //    look up selected homepage articles
   const hpArticles = await getHomepageArticles(hpData.articles);
-  console.log('hpArticles:', hpArticles);
-  //    render correct layout components (above)
+  console.log('found hpArticles keys:', Object.keys(hpArticles));
 
-  const articles = await listAllArticles();
   const tags = await listAllTags();
   const sections = await listAllSections();
 
@@ -125,7 +49,6 @@ export async function getStaticProps() {
     props: {
       hpData,
       hpArticles,
-      articles,
       tags,
       sections,
     },
