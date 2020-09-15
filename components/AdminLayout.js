7a2640eebd2033f692f04db7d1a24d16eb14a30c
Jacqui Lough <lough.jacqui@gmail.com>
2020-09-16 09:41:29 +1000

    Added Authors list page to tinycms (NOTE: requires new ENV vars for admin API)

---
+++ components/AdminLayout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import globalStyles from '../styles/global.js';
+
+export default function AdminLayout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>tinycms</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Libre+Franklin:ital,wght@0,300;0,800;1,300;1,800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <main className="container">{children}</main>
+      <style jsx global>
+        {globalStyles}
+      </style>
+    </>
+  );
+}
