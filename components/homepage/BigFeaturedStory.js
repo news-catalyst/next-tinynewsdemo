7cd1773c56a978bde2a956d800ec122411f2c310
Jacqui Lough <lough.jacqui@gmail.com>
2020-09-16 13:48:04 +1000

    work-in-progress: click to edit homepage, article search form with results

--- components/homepage/BigFeaturedStory.js
+++ components/homepage/BigFeaturedStory.js
@@ -8,8 +8,13 @@ import ArticleLink from './ArticleLink.js';
 import GlobalNav from '../nav/GlobalNav.js';
 import GlobalFooter from '../nav/GlobalFooter.js';
 import FeaturedSidebar from './FeaturedSidebar.js';
+import { searchArticles } from '../../lib/articles.js';
 
 export default function BigFeaturedStory(props) {
+  const [isEditing, setEditing] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   const [featuredArticle, setFeaturedArticle] = useState(
     props.articles['featured']
   );
@@ -18,6 +23,18 @@ export default function BigFeaturedStory(props) {
   );
 
   const isAmp = useAmp();
+
+  async function handleSearch(event) {
+    event.preventDefault();
+    console.log('handling search...', event);
+    const results = await searchArticles(
+      props.apiUrl,
+      props.apiToken,
+      searchTerm
+    );
+    console.log('results: ', results);
+    setSearchResults(results);
+  }
 
   // I noticed that `streamArticles` were null or undefined on occasional page loads
   // using this hook seems to fix the load order issue; the useState calls could probably
@@ -44,7 +61,45 @@ export default function BigFeaturedStory(props) {
         <div className="featured-article">
           <div className="columns">
             <div className="column is-two-thirds">
-              {featuredArticle && (
+              {isEditing ? (
+                <article className="message">
+                  <div className="message-header">
+                    <p>Feature an Article</p>
+                    <button
+                      className="delete"
+                      onClick={() => setEditing(false)}
+                    ></button>
+                  </div>
+                  <div className="message-body">
+                    <form onSubmit={handleSearch}>
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Search by headline"
+                        onChange={(ev) => setSearchTerm(ev.target.value)}
+                      />
+                    </form>
+                    <ul>
+                      {searchResults.map((result) => (
+                        <li key={result.id}>{result.headline.value}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </article>
+              ) : (
+                <div onClick={() => setEditing(true)}>
+                  <div id="featuredArticle">
+                    {featuredArticle && (
+                      <FeaturedArticleLink
+                        key={featuredArticle.id}
+                        article={featuredArticle}
+                        amp={isAmp}
+                      />
+                    )}
+                  </div>
+                </div>
+              )}
+              {!props.editable && featuredArticle && (
                 <FeaturedArticleLink
                   key={featuredArticle.id}
                   article={featuredArticle}
