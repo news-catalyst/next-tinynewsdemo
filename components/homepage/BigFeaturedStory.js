1e79f42dc1305e6de3ad4ac8e94f53cacb6d760c
Jacqui Lough <lough.jacqui@gmail.com>
2020-09-21 07:40:20 +1000

    Fixed poorly merged conflict (by me) issues

--- components/homepage/BigFeaturedStory.js
+++ components/homepage/BigFeaturedStory.js
@@ -7,67 +7,55 @@ import ModalArticleSearch from '../tinycms/ModalArticleSearch';
 export default function BigFeaturedStory(props) {
   const [isModalActive, setModal] = useState(false);
 
-  const isAmp = useAmp();
-
   useEffect(() => {
     props.setFeaturedArticle(props.articles['featured']);
   }, [props.articles]);
 
   return (
     <div className="homepage">
-      <Layout meta={siteMetadata}>
-        <GlobalNav
-          metadata={siteMetadata}
-          tags={props.tags}
-          sections={props.sections}
-        />
-        <div className="featured-article">
-          <div className="columns">
-            <div className="column is-two-thirds">
-              {props.editable && props.featuredArticle && (
-                <>
-                  <ModalArticleSearch
-                    apiUrl={props.apiUrl}
-                    apiToken={props.apiToken}
-                    isActive={isModalActive}
-                    setModal={setModal}
-                    setFeaturedArticle={props.setFeaturedArticle}
-                  />
-
-                  <button
-                    className="button is-info"
-                    onClick={() => setModal(true)}
-                  >
-                    Change Featured Article
-                  </button>
-                  <div id="featuredArticle">
-                    {props.featuredArticle && (
-                      <FeaturedArticleLink
-                        key={props.featuredArticle.id}
-                        article={props.featuredArticle}
-                        amp={isAmp}
-                      />
-                    )}
-                  </div>
-                </>
-              )}
-              {!props.editable && props.featuredArticle && (
-                <FeaturedArticleLink
-                  key={props.featuredArticle.id}
-                  article={props.featuredArticle}
-                  amp={isAmp}
+      <div className="featured-article">
+        <div className="columns">
+          <div className="column is-two-thirds">
+            {props.editable && props.featuredArticle && (
+              <>
+                <ModalArticleSearch
+                  apiUrl={props.apiUrl}
+                  apiToken={props.apiToken}
+                  isActive={isModalActive}
+                  setModal={setModal}
+                  setFeaturedArticle={props.setFeaturedArticle}
                 />
-              )}
-            </div>
-            <div className="column is-one-third">
-              <FeaturedSidebar />
-            </div>
+
+                <button
+                  className="button is-info"
+                  onClick={() => setModal(true)}
+                >
+                  Change Featured Article
+                </button>
+                <div id="featuredArticle">
+                  {props.featuredArticle && (
+                    <FeaturedArticleLink
+                      key={props.featuredArticle.id}
+                      article={props.featuredArticle}
+                      amp={props.isAmp}
+                    />
+                  )}
+                </div>
+              </>
+            )}
+            {!props.editable && props.featuredArticle && (
+              <FeaturedArticleLink
+                key={props.featuredArticle.id}
+                article={props.featuredArticle}
+                amp={isAmp}
+              />
+            )}
           </div>
           <div className="column is-one-third">
             <FeaturedSidebar />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
