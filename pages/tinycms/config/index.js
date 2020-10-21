ca27654a23381c5948cea04b029d75c9be6d2b3c
Jacqui Lough <lough.jacqui@gmail.com>
2020-10-21 14:02:23 +1100

    cleaned up all the unused code in this simple page

--- pages/tinycms/config/index.js
+++ pages/tinycms/config/index.js
@@ -1,35 +1,9 @@
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { listLayoutSchemas } from '../../../lib/homepage.js';
+import React from 'react';
 import AdminLayout from '../../../components/AdminLayout.js';
 import AdminNav from '../../../components/nav/AdminNav';
 
-export default function HomepageLayouts(props) {
-  // const [message, setMessage] = useState(null);
-
-  // const router = useRouter();
-  // const { action } = router.query;
-
-  // useEffect(() => {
-  //   if (action && action === 'edit') {
-  //     setMessage('Successfully updated homepage layout.');
-  //   }
-  //   if (action && action === 'create') {
-  //     setMessage('Successfully created homepage layout.');
-  //   }
-  // }, []);
-
-  // const listItems = homepageLayouts.map((homepageLayout) => {
-  //   return (
-  //     <li key={homepageLayout.id}>
-  //       <Link key={`${homepageLayout.id}-link`} href={`/tinycms/config/homepage-layouts/${homepageLayout.id}`}>
-  //         <a>{homepageLayout.name}</a>
-  //       </Link>
-  //     </li>
-  //   );
-  // });
-
+export default function Config(props) {
   return (
     <AdminLayout>
       <AdminNav homePageEditor={false} />
@@ -49,12 +23,3 @@ export default function HomepageLayouts(props) {
     </AdminLayout>
   );
 }
-
-// export async function getStaticProps() {
-//   let homepageLayouts = await listLayoutSchemas();
-//   return {
-//     props: {
-//       homepageLayouts: homepageLayouts,
-//     },
-//   };
-// }
