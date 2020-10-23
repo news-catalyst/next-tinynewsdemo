0e7eb5466b79abb5555422feebbb211b71507eb6
Jacqui Lough <lough.jacqui@gmail.com>
2020-10-23 13:47:14 +1100

    tinycms categories require title and slug

--- components/tinycms/Notification.js
+++ components/tinycms/Notification.js
@@ -1,11 +1,20 @@
 export default function Notification(props) {
+  let messages = props.message;
+  if (typeof props.message === 'string') {
+    messages = [props.message];
+  }
   return (
     <div className={`notification is-${props.notificationType}`}>
       <button
         className="delete"
         onClick={() => props.setShowNotification(false)}
       ></button>
-      {props.message}
+      {messages.map((msg) => (
+        <div key={msg}>
+          {msg}
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
