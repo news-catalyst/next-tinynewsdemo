8246db8e0de35d54da21b91db07592cdaa0751cf
Jacqui Lough <lough.jacqui@gmail.com>
2020-12-21 15:59:44 +1100

    Made the title and locale switcher into one component for tinycms

---
+++ components/tinycms/AdminHeader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import LocaleSwitcher from './LocaleSwitcher';
+
+export default function AdminHeader(props) {
+  return (
+    <div>
+      <div className="level-left">
+        <div className="level-item">
+          <h1 className="title">
+            {props.title} ({props.currentLocale.code})
+          </h1>
+        </div>
+      </div>
+      <div className="level-right">
+        <div className="level-item">
+          <LocaleSwitcher
+            locales={props.locales}
+            currentLocale={props.currentLocale}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
