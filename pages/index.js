9acee11396f76f78a3debab2b5fe597654aeb0b6
Tyler Fisher <tyler@tylerjfisher.com>
2021-01-26 16:42:55 -0500

    Pass ads to all pages with article streams

--- pages/index.js
+++ pages/index.js
@@ -136,7 +136,6 @@ export async function getStaticProps({ locale }) {
 
   const allAds = await cachedContents('ads', getArticleAds);
   const expandedAds = allAds.filter((ad) => ad.adTypeId === 166);
-  console.log(expandedAds);
 
   return {
     props: {
