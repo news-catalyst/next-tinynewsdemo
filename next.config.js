a22931acc23cc23051ef3a1774db320be66f529e
Jacqui Lough <lough.jacqui@gmail.com>
2021-01-28 09:12:15 +1100

    Added new s3 bucket for shared assets to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   images: {
     domains: [
+      'tnc-test-upload-bucket.s3.amazonaws.com',
       'tiny-news-demo-assets-dev.s3.amazonaws.com',
       'wherebyspace.nyc3.digitaloceanspaces.com',
       'tiny-news-demo-assets-oaklyn.s3.amazonaws.com',
