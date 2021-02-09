793b32d6e7bf2cf228e496dafdbe4c2f909de85d
Jacqui Lough <lough.jacqui@gmail.com>
2021-02-09 14:56:33 +1100

    Preview page for articles backed by hasura should show most recently updated content, published or not

--- pages/preview/[category]/[slug].js
+++ pages/preview/[category]/[slug].js
@@ -1,12 +1,9 @@
 import DefaultErrorPage from 'next/error';
 import {
-  listAllLocales,
-  getArticleBySlug,
-  listAllArticleSlugs,
-  listAllSections,
-  listAllTags,
+  hasuraListAllArticleSlugs,
+  hasuraPreviewArticlePage,
 } from '../../../lib/articles.js';
-import { getSiteMetadataForLocale } from '../../../lib/site_metadata.js';
+import { hasuraLocaliseText } from '../../../lib/utils.js';
 import { getArticleAds } from '../../../lib/ads.js';
 import { cachedContents } from '../../../lib/cached';
 import Article from '../../../components/Article.js';
@@ -23,8 +20,24 @@ export default function PreviewArticle(props) {
   return <Article {...props} />;
 }
 
-export async function getStaticPaths({ locales }) {
-  const paths = await listAllArticleSlugs(locales);
+export async function getStaticPaths() {
+  const { errors, data } = await hasuraListAllArticleSlugs();
+  if (errors) {
+    throw errors;
+  }
+
+  let paths = [];
+  for (const article of data.articles) {
+    for (const locale of article.article_translations) {
+      paths.push({
+        params: {
+          category: article.category.slug,
+          slug: article.slug,
+        },
+        locale: locale.locale_code,
+      });
+    }
+  }
   return {
     paths,
     fallback: false,
@@ -32,31 +45,73 @@ export async function getStaticPaths({ locales }) {
 }
 
 export async function getStaticProps({ locale, params }) {
-  const localeMappings = await cachedContents('locales', listAllLocales);
+  const apiUrl = process.env.HASURA_API_URL;
+  const apiToken = process.env.ORG_SLUG;
 
-  const currentLocale = localeMappings.find(
-    (localeMap) => localeMap.code === locale
-  );
+  let article = {};
+  let sectionArticles = [];
+  let sections = [];
+  let tags = [];
+  let siteMetadata;
 
-  const article = await getArticleBySlug(
-    currentLocale,
-    params.slug,
-    process.env.CONTENT_DELIVERY_API_URL
-  );
-  const sections = await cachedContents('sections', listAllSections);
-  const tags = await cachedContents('tags', listAllTags);
+  const { errors, data } = await hasuraPreviewArticlePage({
+    url: apiUrl,
+    orgSlug: apiToken,
+    localeCode: locale,
+    categorySlug: params.category,
+  });
+  if (errors || !data) {
+    console.log('error gettig article page:', errors);
+    return {
+      notFound: true,
+    };
+    // throw errors;
+  } else {
+    tags = data.tags;
+    for (var i = 0; i < tags.length; i++) {
+      tags[i].title = hasuraLocaliseText(tags[i].tag_translations, 'title');
+    }
+    sections = data.categories;
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].title = hasuraLocaliseText(
+        sections[i].category_translations,
+        'title'
+      );
+    }
+
+    article = data.articles.find((a) => a.slug === params.slug);
+    if (article && article.article_translations.length > 1) {
+      // let mostRecentContent = article.article_translations.map(function(e) { return e.updated_at; }).sort().reverse()[0]
+      let mostRecentContents = article.article_translations.sort((a, b) => {
+        return new Date(b.updated_at) - new Date(a.updated_at);
+      });
+      let mostRecentContent = mostRecentContents[0];
+
+      console.log('mostRecentContent:', mostRecentContent.updated_at);
+
+      article.article_translations = [mostRecentContent];
+    }
+    sectionArticles = data.articles.filter((a) => a.slug !== params.slug);
+
+    let metadatas = data.site_metadatas;
+    try {
+      siteMetadata = metadatas[0].site_metadata_translations[0].data;
+    } catch (err) {
+      console.log('failed finding site metadata for ', locale, metadatas);
+    }
+  }
+
   const allAds = await cachedContents('ads', getArticleAds);
   const ads = allAds.filter((ad) => ad.adTypeId === 164);
-  const siteMetadata = await getSiteMetadataForLocale(currentLocale);
 
   return {
     props: {
-      ads,
       article,
-      currentLocale,
       sections,
-      tags,
+      ads,
       siteMetadata,
+      sectionArticles,
+      tags,
     },
   };
 }
