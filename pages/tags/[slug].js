ccd25f5f3275813ccbecc58ac7b74e707e818516
Jacqui Lough <lough.jacqui@gmail.com>
2021-02-09 12:12:46 +1100

    hasura-backed tag index pages

--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -1,15 +1,9 @@
+import { useRouter } from 'next/router';
 import Layout from '../../components/Layout.js';
-import {
-  listAllLocales,
-  listAllArticlesByTag,
-  listAllSections,
-  listAllTagPaths,
-  getTagBySlug,
-} from '../../lib/articles.js';
-import { getSiteMetadataForLocale } from '../../lib/site_metadata.js';
+import { listAllTagPaths, hasuraTagPage } from '../../lib/articles.js';
 import { cachedContents } from '../../lib/cached';
 import { getArticleAds } from '../../lib/ads.js';
-import { localiseText } from '../../lib/utils.js';
+import { hasuraLocaliseText } from '../../lib/utils.js';
 import { useAmp } from 'next/amp';
 import ArticleStream from '../../components/homepage/ArticleStream';
 
@@ -17,21 +11,27 @@ export default function TagPage({
   articles,
   tag,
   sections,
-  currentLocale,
   siteMetadata,
   expandedAds,
 }) {
+  const router = useRouter();
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   const isAmp = useAmp();
-  let tagTitle = localiseText(currentLocale, tag.title);
+
+  let tagTitle = hasuraLocaliseText(tag.tag_translations, 'title');
   return (
-    <Layout meta={siteMetadata} sections={sections} locale={currentLocale}>
+    <Layout meta={siteMetadata} sections={sections}>
       <ArticleStream
         articles={articles}
         sections={sections}
         showCategory={true}
         isAmp={isAmp}
         title={`Articles tagged with ${tagTitle}`}
-        locale={currentLocale}
         metadata={siteMetadata}
         ads={expandedAds}
       />
@@ -48,23 +48,52 @@ export async function getStaticPaths({ locales }) {
 }
 
 export async function getStaticProps({ locale, params }) {
-  const localeMappings = await cachedContents('locales', listAllLocales);
+  const apiUrl = process.env.HASURA_API_URL;
+  const apiToken = process.env.ORG_SLUG;
 
-  const currentLocale = localeMappings.find(
-    (localeMap) => localeMap.code === locale
-  );
+  let articles = [];
+  let sections = [];
+  let tag;
+  let siteMetadata;
 
-  const siteMetadata = await getSiteMetadataForLocale(currentLocale);
+  const { errors, data } = await hasuraTagPage({
+    url: apiUrl,
+    orgSlug: apiToken,
+    tagSlug: params.slug,
+    localeCode: locale,
+  });
 
-  const articles = await listAllArticlesByTag(currentLocale, params.slug);
-  const sections = await cachedContents('sections', listAllSections);
-  const tag = await getTagBySlug(params.slug);
+  if (errors || !data) {
+    return {
+      notFound: true,
+    };
+  } else {
+    tag = data.tags[0];
+    tag.tag_articles.map((tag_article) => {
+      articles.push(tag_article.article);
+    });
+
+    sections = data.categories;
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].title = hasuraLocaliseText(
+        sections[i].category_translations,
+        'title'
+      );
+    }
+
+    let metadatas = data.site_metadatas;
+    try {
+      siteMetadata = metadatas[0].site_metadata_translations[0].data;
+    } catch (err) {
+      console.log('failed finding site metadata for ', locale, metadatas);
+    }
+  }
+
   const allAds = await cachedContents('ads', getArticleAds);
   const expandedAds = allAds.filter((ad) => ad.adTypeId === 166);
 
   return {
     props: {
-      currentLocale,
       articles,
       tag,
       sections,
