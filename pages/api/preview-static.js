842ee062b05841532a33e3746fe4c9ee18ba35a8
Jacqui Lough <lough.jacqui@gmail.com>
2021-02-12 09:47:56 +1100

    code cleanup

--- pages/api/preview-static.js
+++ pages/api/preview-static.js
@@ -11,10 +11,6 @@ export default async (req, res) => {
     const apiUrl = process.env.HASURA_API_URL;
     const apiToken = process.env.ORG_SLUG;
 
-    let page = {};
-    let sections;
-    let siteMetadata = {};
-
     const { errors, data } = await hasuraGetPage({
       url: apiUrl,
       orgSlug: apiToken,
@@ -23,29 +19,11 @@ export default async (req, res) => {
     });
     if (errors || !data) {
       return res.status(401).json({ message: 'Invalid slug' });
-      // } else {
-      //   console.log(data);
-      //   sections = data.categories;
-      //   page = data.pages[0];
-      //   siteMetadata = data.site_metadatas[0].site_metadata_translations[0].data;
-      //   for (var i = 0; i < sections.length; i++) {
-      //     sections[i].title = hasuraLocaliseText(
-      //       sections[i].category_translations,
-      //       'title'
-      //     );
-      //   }
     }
   }
 
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
-
-  // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-
-  // BUG: this line of code taken from the nextjs docs doesn't work - throws a typeerror: res.redirect is not a function (??)
-  // (https://nextjs.org/docs/advanced-features/preview-mode)
-  // res.redirect(article.slug)
 
   const nextPath = '/' + req.query.slug;
 
