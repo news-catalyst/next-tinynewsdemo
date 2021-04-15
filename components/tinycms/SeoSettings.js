02f676c29297299973fdbcedac1723ef589760ba
Jacqui Lough <lough.jacqui@gmail.com>
2021-04-15 13:50:10 +1000

    removing unused components for tinycms settings that turn out to be unnecessary

--- components/tinycms/SeoSettings.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import tw from 'twin.macro';
-
-const SettingsHeader = tw.h1`text-4xl font-normal leading-normal mt-0 mb-2 text-black`;
-
-export default function SeoSettings(props) {
-  return <SettingsHeader>SEO/Social Settings</SettingsHeader>;
-}
