6f25a7256d2967943fc94230bcd9a2f6328bdf50
Jacqui Lough <lough.jacqui@gmail.com>
2021-04-15 13:50:10 +1000

    removing unused components for tinycms settings that turn out to be unnecessary

--- components/tinycms/MembershipSettings.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import tw from 'twin.macro';
-
-const SettingsHeader = tw.h1`text-4xl font-normal leading-normal mt-0 mb-2 text-black`;
-
-export default function MembershipSettings(props) {
-  return <SettingsHeader>Membership Settings</SettingsHeader>;
-}
