6805aba59a1774bd458b7184abf4b5ad42cbff38
Jacqui Lough <lough.jacqui@gmail.com>
2021-04-16 11:59:50 +1000

    redesign sections, tags and notification success/error alert for tailwind

--- pages/tinycms/sections/[id].js
+++ pages/tinycms/sections/[id].js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import tw, { css, styled } from 'twin.macro';
 import AdminLayout from '../../../components/AdminLayout';
 import {
   hasuraGetSectionById,
   hasuraUpdateSection,
 } from '../../../lib/section';
 import AdminNav from '../../../components/nav/AdminNav';
-import AdminHeader from '../../../components/tinycms/AdminHeader';
 import Notification from '../../../components/tinycms/Notification';
 import { hasuraLocaliseText } from '../../../lib/utils';
+
+const Input = styled.input`
+  ${tw`mb-5 px-3 py-3 placeholder-gray-300 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full`}
+`;
+const SubmitButton = tw.input`hidden md:flex w-full md:w-auto px-4 py-2 text-right bg-green-700 hover:bg-green-500 text-white md:rounded`;
+const CancelButton = tw.button`hidden md:flex w-full md:w-auto px-4 py-2 text-right bg-gray-600 hover:bg-gray-300 text-white md:rounded`;
 
 export default function EditSection({
   apiUrl,
@@ -88,82 +94,47 @@ export default function EditSection({
         />
       )}
 
-      <div id="page">
-        <AdminHeader
-          locales={locales}
-          currentLocale={currentLocale}
-          title="Edit Section"
-          id={section.id}
-        />
+      <div tw="container mx-auto min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">
+        <div tw="pt-5 pb-10">
+          <h1 tw="inline-block text-3xl font-extrabold text-gray-900 tracking-tight">
+            Edit Section
+          </h1>
+        </div>
 
         <form onSubmit={handleSubmit}>
-          <div className="field">
-            <label className="label" htmlFor="title">
-              Title
-            </label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                value={title}
-                name="title"
-                onChange={(ev) => setTitle(ev.target.value)}
-              />
-            </div>
-          </div>
+          <label htmlFor="title">
+            <span tw="block font-medium text-gray-700">Title</span>
+            <Input
+              type="text"
+              value={title}
+              name="title"
+              onChange={(ev) => setTitle(ev.target.value)}
+            />
+          </label>
 
-          <div className="field">
-            <label className="label" htmlFor="slug">
-              Slug
-            </label>
-            <div className="control">
-              <input
-                className="input"
-                type="text"
-                value={slug}
-                name="slug"
-                onChange={(ev) => setSlug(ev.target.value)}
-              />
-            </div>
-          </div>
+          <label htmlFor="slug">
+            <span tw="block font-medium text-gray-700">Slug</span>
+            <Input
+              type="text"
+              value={slug}
+              name="slug"
+              onChange={(ev) => setSlug(ev.target.value)}
+            />
+          </label>
 
-          <div className="field">
-            <label className="checkbox" htmlFor="slug">
-              <input
-                name="published"
-                type="checkbox"
-                checked={published}
-                onChange={handlePublished}
-              />
-              Published
-            </label>
-          </div>
+          <label tw="mt-5" htmlFor="published">
+            <input
+              name="published"
+              type="checkbox"
+              checked={published}
+              onChange={handlePublished}
+            />
+            <span tw="ml-5 font-medium text-gray-700">Published</span>
+          </label>
 
-          <div className="level">
-            <div className="level-left">
-              <div className="level-item">
-                <div className="control">
-                  <input
-                    className="button is-link"
-                    name="submit"
-                    type="submit"
-                    value="Submit"
-                  />
-                </div>
-                <div className="control">
-                  <button
-                    className="button is-link is-light"
-                    name="cancel"
-                    onClick={handleCancel}
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="level-right">
-              <div className="level-item"></div>
-            </div>
+          <div tw="grid grid-cols-4 gap-24 mt-4">
+            <SubmitButton type="submit" value="Submit" />
+            <CancelButton>Cancel</CancelButton>
           </div>
         </form>
       </div>
