0ba619c8a015d4ceb136c33100446a3bd097505d
Jacqui Lough <lough.jacqui@gmail.com>
2021-04-19 12:06:38 +1000

    redesign rollout for analytics: sessions stats

--- components/tinycms/analytics/ReferralSource.js
+++ components/tinycms/analytics/ReferralSource.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
+import tw from 'twin.macro';
 import { getMetricsData } from '../../../lib/analytics';
+
+const SubHeaderContainer = tw.div`pt-10 pb-5`;
+const SubHeader = tw.h1`inline-block text-xl font-extrabold text-gray-900 tracking-tight`;
 
 const ReferralSource = (props) => {
   const referralRef = useRef();
@@ -54,33 +58,36 @@ const ReferralSource = (props) => {
   }, [props.startDate, props.endDate]);
 
   return (
-    <section className="section" id="referral" ref={referralRef}>
-      <div className="content">
-        <h2 className="subtitle">Sessions by referral source</h2>
+    <>
+      <SubHeaderContainer ref={referralRef}>
+        <SubHeader>Session by referral source</SubHeader>
+      </SubHeaderContainer>
+      <p tw="p-2">
+        {props.startDate.format('dddd, MMMM Do YYYY')} -{' '}
+        {props.endDate.format('dddd, MMMM Do YYYY')}
+      </p>
 
-        <p className="content">
-          {props.startDate.format('dddd, MMMM Do YYYY')} -{' '}
-          {props.endDate.format('dddd, MMMM Do YYYY')}
-        </p>
-
-        <table className="table is-fullwidth" style={{ width: '100%' }}>
-          <thead>
-            <tr>
-              <th>Subscriber</th>
-              <th>Sessions</th>
+      <table tw="w-full table-auto">
+        <thead>
+          <tr>
+            <th tw="px-4">Subscriber</th>
+            <th tw="px-4">Sessions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {referralData.labels.map((label, i) => (
+            <tr key={`referral-data-row-${i}`}>
+              <td tw="border border-gray-500 px-4 py-2 text-gray-600 font-medium">
+                {label}
+              </td>
+              <td tw="border border-gray-500 px-4 py-2 text-gray-600 font-medium">
+                {referralData.values[i]}
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {referralData.labels.map((label, i) => (
-              <tr key={`referral-data-row-${i}`}>
-                <td>{label}</td>
-                <td>{referralData.values[i]}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </section>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
