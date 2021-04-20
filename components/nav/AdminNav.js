edce8ef6260203fff6c097974cef38b5b7c840c7
Tyler Fisher <tyler@tylerjfisher.com>
2021-04-20 11:24:04 -0400

    Add cursor: pointer to admin nav

--- components/nav/AdminNav.js
+++ components/nav/AdminNav.js
@@ -13,7 +13,7 @@ const BrandContainer = tw.div`w-full md:w-1/5 text-center md:text-left font-bold
 
 const NavItemsDiv = tw.div`flex justify-center items-center w-full md:w-4/5`;
 const HomepageNavItemsDiv = tw.div`flex justify-end items-center w-full md:w-3/5`;
-const NavItem = tw.a`mx-4`;
+const NavItem = tw.a`mx-4 cursor-pointer`;
 const SignoutButton = tw.a`hidden md:flex h-10 w-full md:w-auto px-4 py-2 text-right bg-blue-900 hover:bg-blue-500 text-white md:rounded`;
 
 export default function NewAdminNav(props) {
