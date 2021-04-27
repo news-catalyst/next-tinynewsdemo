416c71a34f025cc3b3c5ca31631952be3cf14297
Jacqui Lough <lough.jacqui@gmail.com>
2021-04-27 12:55:30 +1000

    Initial version of yarn bootstrap that configures a new project in vercel: done

--- script/vercel.js
+++ script/vercel.js
@@ -4,27 +4,47 @@ require('dotenv').config({ path: '.env.local' })
 const TOKEN = process.env.VERCEL_TOKEN;
 const BASE_URL = "https://api.vercel.com/v6";
 const ORG_NAME= process.env.ORG_SLUG;
+const GIT_REPO = process.env.GIT_REPO;
 
 async function createProject() {
 
   let projectId;
+  let domain;
 
-  let url = BASE_URL + "/projects";
 
   let requestHeaders = {
     "Authorization": `Bearer ${TOKEN}`,
     "Content-Type": "application/json"
   }
 
+  let teamsUrl = BASE_URL + "/teams";
+  let teamsResult = await fetch(teamsUrl, {
+    method: "GET",
+    headers: requestHeaders,
+  })
+  let teamsData = await teamsResult.json();
+  let ncTeam = teamsData.teams.find((data) => data.slug === 'news-catalyst');
+
+  let teamId = ncTeam.id;
+  console.log("news catalyst team id: ", teamId)
+
+  let url = BASE_URL + "/projects?teamId=" + teamId;
+
   console.log("creating vercel project with name:", ORG_NAME)
 
+  let requestBody = JSON.stringify({
+        name: ORG_NAME,
+        gitRepository: {
+          type: 'github',
+          repo: GIT_REPO,
+        }
+      });
+      console.log(requestBody);
   try {
     const response = await fetch(url, {
       method: 'POST',
       headers: requestHeaders,
-      body: JSON.stringify({
-        name: ORG_NAME
-      }),
+      body: requestBody
     });
 
     const data = await response.json();
@@ -33,34 +53,49 @@ async function createProject() {
     if (data && data.error) {
       console.error("[" + data.error.code + "] Unable to create Vercel project because: " + data.error.message);
     } else {
-      console.log("Created Vercel project at domain: " + data.alias[0].domain);
+      domain = data.alias[0].domain;
+      console.log("Created Vercel project for " + ORG_NAME);
 
       projectId = data.id;
-      let envUrl = BASE_URL + "/projects/" + projectId + "/env";
+
+      let projectUrl = BASE_URL + "/projects/" + projectId + "?teamId=" + teamId;
+      const updateResponse = await fetch(projectUrl, {
+        method: 'PATCH',
+        headers: requestHeaders,
+        body: JSON.stringify({
+          framework: "nextjs",
+          buildCommand: "yarn clear && yarn populate && yarn build"
+
+        })
+      });
+      const updateResponseData = await updateResponse.json();
+      if (updateResponseData.error) {
+        console.error("Failed configuring the project for nextjs with a custom build command: ", updateResponseData.error);
+      }
+      let envUrl = projectUrl + "/env?teamId=" + teamId;
 
       const currentEnv = require('dotenv').config({ path: '.env.local' });
       let envData = currentEnv.parsed;
 
-      Object.keys(envData).map((key) => {
-        let requestData = {
-          "type": "encrypted",
-          "key": key,
-          "value": envData[key],
-          "target": [
-            "production",
-            "preview"
-          ]
-        };
-        fetch(envUrl, {
+      const envRequestPromises = Object.keys(envData).map(key => fetch(envUrl, {
           method: 'POST',
           headers: requestHeaders,
-          body: JSON.stringify(requestData),
+          body: JSON.stringify({
+            "type": "encrypted",
+            "key": key,
+            "value": envData[key],
+            "target": [
+              "production",
+              "preview"
+            ]
+          })
         })
-        .then((res) => {
-          console.log("- set env var " + key);
-        })
-        .catch(console.error);
-      })
+      );
+      
+      const envResults = await Promise.all(envRequestPromises);
+
+      console.log();
+      console.log("Done! The project is configured and all environment variables from .env.local are setup. To deploy, push to the master branch on github");
     }
   } catch (error) {
     console.log("error creating project in vercel:", error);
