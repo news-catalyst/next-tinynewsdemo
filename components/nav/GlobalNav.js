4f82df73182f28daaeddee6c5d1d3898cd81a731
Tyler Fisher <tyler@tylerjfisher.com>
2021-04-27 15:13:05 -0400

    Global nav alignment

--- components/nav/GlobalNav.js
+++ components/nav/GlobalNav.js
@@ -9,7 +9,7 @@ const NavInnerContainer = tw.div`lg:p-5 flex flex-wrap flex-row mx-auto max-w-7x
 const NavHeader = tw.h1`text-4xl leading-none font-bold ml-4 lg:ml-0 flex-1 order-1`;
 const RightNav = tw.nav`lg:text-right lg:flex-1 flex flex-row flex-nowrap mt-5 order-3 lg:order-none overflow-y-hidden overflow-x-scroll w-full flex-grow border-t border-gray-200 lg:border-t-0 lg:w-auto lg:block lg:mt-0`;
 const SectionLink = styled.a(({ meta }) => ({
-  ...tw`lg:items-center lg:mr-8 lg:pt-2 inline-flex items-end h-full py-2 px-5 lg:pb-0 lg:px-0`,
+  ...tw`lg:items-center lg:mr-8 lg:py-0 inline-flex items-end h-full py-2 px-5 lg:pb-0 lg:px-0`,
   fontFamily: Typography[meta.theme].SectionLink,
 }));
 
