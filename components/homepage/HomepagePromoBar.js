f2fbd60ae9bd3630690babf65ffb32de4a4afe58
Tyler Fisher <tyler@tylerjfisher.com>
2021-04-27 15:02:00 -0400

    Support predefined color schemes

--- components/homepage/HomepagePromoBar.js
+++ components/homepage/HomepagePromoBar.js
@@ -1,8 +1,14 @@
 import Link from 'next/link';
 import tw, { styled } from 'twin.macro';
 import Typography from '../common/Typography';
+import Colors from '../common/Colors';
 
-const SectionLayout = tw.section`bg-gray-100 pt-8 px-5 mb-16`;
+const SectionLayout = styled.section(({ meta }) => ({
+  ...tw`pt-8 px-5 mb-16`,
+  backgroundColor: Colors[meta.color].HomepagePromoBackground,
+  color: Colors[meta.color].HomepagePromoText,
+}));
+
 const SectionContainer = tw.div`px-5 flex flex-row flex-wrap mx-auto w-full max-w-7xl`;
 const Block = tw.div`w-full md:w-1/2 mb-8`;
 const LeftBlock = tw(
@@ -10,24 +16,28 @@ const LeftBlock = tw(
 )`border-b md:border-r md:border-b-0 pb-8 mb-8 md:pb-0 md:mb-0 border-gray-200 md:pr-4`;
 const RightBlock = tw(Block)`md:pl-4`;
 const BlockHeader = styled.h2(({ meta }) => ({
-  ...tw`text-lg font-bold mb-5`,
+  ...tw`text-xl font-bold mb-5`,
   fontFamily: Typography[meta.theme].HomepagePromoBlockHeader,
 }));
 const BlockDek = styled.p(({ meta }) => ({
-  ...tw`text-sm mb-3`,
+  ...tw`text-base mb-3`,
   fontFamily: Typography[meta.theme].HomepagePromoBlockDek,
 }));
 const BlockCTA = styled.a(({ meta }) => ({
-  ...tw`text-sm font-bold`,
+  ...tw`text-base font-bold cursor-pointer`,
   fontFamily: Typography[meta.theme].HomepagePromoBlockCTA,
+  color: Colors[meta.color].CTABackground,
 }));
-const DonateBlockCTA = tw(
-  BlockCTA
-)`inline-flex bg-black text-white items-center px-5`;
+const DonateBlockCTA = styled.a(({ meta }) => ({
+  ...tw`inline-flex text-base font-bold cursor-pointer items-center px-5`,
+  fontFamily: Typography[meta.theme].HomepagePromoBlockCTA,
+  color: Colors[meta.color].CTAText,
+  backgroundColor: Colors[meta.color].CTABackground,
+}));
 
 export default function HomepagePromoBar({ metadata }) {
   return (
-    <SectionLayout>
+    <SectionLayout meta={metadata}>
       <SectionContainer>
         <LeftBlock>
           <BlockHeader meta={metadata}>{metadata.aboutHed}</BlockHeader>
--- components/nav/Donate.js
+++ components/nav/Donate.js
@@ -2,10 +2,13 @@ import Link from 'next/link';
 import { useAnalytics } from '../../lib/hooks/useAnalytics.js';
 import tw, { styled } from 'twin.macro';
 import Typography from '../common/Typography';
+import Colors from '../common/Colors';
 
 const DonateLink = styled.a(({ meta }) => ({
-  ...tw`items-center bg-black text-white flex font-bold leading-4 pt-0.5 px-5 ml-5 order-2 lg:ml-0 lg:order-none`,
+  ...tw`items-center flex font-bold leading-none px-5 ml-5 order-2 lg:ml-0 lg:order-none`,
   fontFamily: Typography[meta.theme].DonateLink,
+  color: Colors[meta.color].CTAText,
+  backgroundColor: Colors[meta.color].CTABackground,
 }));
 
 const Donate = ({ label, url, metadata }) => {
