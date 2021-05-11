c03ef298455e7aa12a8cfd4dc77fff9da2e7f0f3
Tyler Fisher <tyler@tylerjfisher.com>
2021-05-11 16:54:17 -0400

    Don't make the build dependent on the ad API. Closes #551

--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -120,7 +120,7 @@ export async function getStaticProps({ locale, params }) {
     }
   }
 
-  const allAds = await cachedContents('ads', getArticleAds);
+  const allAds = (await cachedContents('ads', getArticleAds)) || [];
   const expandedAds = allAds.filter((ad) => ad.adTypeId === 166);
 
   return {
