61e647e9b201ead47d30115d654ca7ce6a5431c7
Jacqui Lough <lough.jacqui@gmail.com>
2021-05-19 09:05:37 +1000

    updated data import to take start & end date args, iterate over each date in range, default to yesterday->today

--- script/data-import.js
+++ script/data-import.js
@@ -1,17 +1,34 @@
+const { program } = require('commander');
+program.version('0.0.1');
+
 const fetch = require("node-fetch");
 require('dotenv').config({ path: '.env.local' })
 
 const {format} = require('date-fns');
 
 const baseURL = process.env.SITE_URL + "/api/import/";
-const endpoints = ["donors", "geo-sessions", "newsletter-impressions", "newsletters", "page-views", "reading-depth", "reading-frequency", "referral-sessions", "session-duration", "sessions", "subscribers"];
+const endpoints = [
+  "donate-clicks", 
+  "donor-reading-frequency", 
+  "donors", 
+  "geo-sessions", 
+  "newsletter-impressions", 
+  "newsletters", 
+  "page-views", 
+  "reading-depth", 
+  "reading-frequency", 
+  "referral-sessions", 
+  "session-duration", 
+  "sessions", 
+  "subscribers"
+];
 
 async function runDataImport(startDate, endDate) {
   console.log("running data import:", startDate, endDate);
 
   for await (let endpoint of endpoints) {
     let endpointURL = baseURL + endpoint;
-    endpointURL += `?startDate=${startDate}&endDate=${endDate}`;
+    endpointURL += `?startDate=${format(startDate, 'yyyy-MM-dd')}&endDate=${format(endDate, 'yyyy-MM-dd')}`;
 
     console.log(endpointURL);
 
@@ -30,12 +47,34 @@ async function runDataImport(startDate, endDate) {
   };
 }
 
-let twoDaysAgo = new Date(); 
-twoDaysAgo.setDate(twoDaysAgo.getDate() - 2); 
-let startDate = format(twoDaysAgo, "yyyy-MM-dd")
+program
+  .option('-s, --start-date <startDate>', 'start of the date range')
+  .option('-e, --end-date <endDate>', 'end of the date range')
+  .description("imports daily GA data for each date in the specified range")
+  .action( (opts) => {
+    let startDate;
+    if (opts.startDate === undefined) {
+      let yesterday = new Date(); 
+      startDate = new Date(yesterday.setDate(yesterday.getDate() - 1));
+    } else {
+      startDate = new Date(opts.startDate);
+    }
 
-let yesterday = new Date(); // Today!
-yesterday.setDate(yesterday.getDate() - 1); 
-let endDate = format(yesterday, "yyyy-MM-dd")
+    let endDate;
+    if (opts.endDate === undefined) {
+      let today = new Date(); 
+      endDate = new Date(today.setDate(today.getDate() - 1));
+    } else {
+      endDate = new Date(opts.endDate);
+    }
 
-runDataImport(startDate, endDate);
+    for (var d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+      let sd = new Date(d);
+      let ed = new Date(d.setDate(d.getDate() + 1));
+      // console.log("running data import for", sd, ed);
+      runDataImport(sd, ed);
+    }
+
+  });
+
+program.parse(process.argv);
