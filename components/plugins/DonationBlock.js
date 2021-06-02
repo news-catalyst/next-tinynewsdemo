64a8e7240e707dfaad54a5d5ba337316c10e71c5
Tyler Fisher <tyler@tylerjfisher.com>
2021-06-02 15:11:22 -0400

    Switchable promotion blocks

--- components/plugins/DonationBlock.js
+++ components/plugins/DonationBlock.js
@@ -1,19 +1,41 @@
-import Donate from '../nav/Donate';
+import { useEffect, useState } from 'react';
+import tw, { styled } from 'twin.macro';
+import Donate from '../nav/Donate.js';
+import Colors from '../common/Colors';
+import { determineTextColor } from '../../lib/utils';
 
-export default function DonationBlock({ metadata, wrap = true }) {
-  const block = (
-    <div className={`newsletter ${!wrap && 'block'}`}>
-      <h4>{metadata.donateBlockHed}</h4>
-      <p>{metadata.donateBlockDek}</p>
-      <br />
-      <Donate
-        style={{ backgroundColor: '#fff', color: '#000' }}
-        label={metadata.supportCTA}
-        url={metadata.supportURL}
-        metadata={metadata}
-      />
-    </div>
+const DonationWrapper = styled.div(({ textColor, backgroundColor }) => ({
+  ...tw`bg-gray-200 mb-8 pt-7 px-4 pb-8 md:sticky md:top-10`,
+  backgroundColor: backgroundColor,
+  color: textColor,
+}));
+
+const DonationHed = tw.h4`text-2xl font-bold tracking-tight leading-5 mb-2`;
+const DonationDek = tw.p`mb-6`;
+
+export default function DonationBlock({ metadata }) {
+  const [textColor, setTextColor] = useState(null);
+  const [backgroundColor, setBackgroundColor] = useState(null);
+
+  useEffect(() => {
+    let bgc;
+    let tc;
+    if (metadata.color === 'custom') {
+      bgc = metadata.primaryColor;
+      tc = determineTextColor(metadata.primaryColor);
+    } else if (Colors[metadata.color]) {
+      tc = Colors[metadata.color].PromoBlockText;
+      bgc = Colors[metadata.color].PromoBlockBackground;
+    }
+    setBackgroundColor(bgc);
+    setTextColor(tc);
+  }, [metadata]);
+
+  return (
+    <DonationWrapper textColor={textColor} backgroundColor={backgroundColor}>
+      <DonationHed>{metadata.membershipHed}</DonationHed>
+      <DonationDek>{metadata.membershipDek}</DonationDek>
+      <Donate metadata={metadata} label="Donate" />
+    </DonationWrapper>
   );
-
-  return wrap ? <div className="block">{block}</div> : block;
 }
