8d47691cf04897bda96ecfe0121e6ba3b7092738
Jacqui Lough <lough.jacqui@gmail.com>
2021-06-08 07:39:07 +1000

    check PREVIEW_DEPLOY var to allow preview deployments

--- script/build-vercel.sh
+++ script/build-vercel.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "VERCEL_ENV: $VERCEL_ENV"
+echo "PREVIEW_DEPLOY: $PREVIEW_DEPLOY"
 
-if [[ "$VERCEL_ENV" == "production" ]] ; then
+if [[ "$VERCEL_ENV" == "production" || "$PREVIEW_DEPLOY" == "1" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
 
 else
   # Don't build
-  echo "🛑 - Build cancelled: not production"
+  echo "🛑 - Build cancelled: preview deploys are turned off (set PREVIEW_DEPLOY to '1' to turn on)"
   exit 0;
 fi
