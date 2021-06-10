9dd5210af5beabf994518aa3cf971ee4644d99b9
Jacqui Lough <lough.jacqui@gmail.com>
2021-06-10 11:28:29 +1000

    test for section and article featured link

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -2,5 +2,7 @@ describe('Home', () => {
   it('renders successfully', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('Oaklyn')
+    cy.get('header nav a[href="/business"]')
+    cy.get('a.featured')
   })
 })
