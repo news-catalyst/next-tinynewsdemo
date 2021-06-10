87f94f48c5d67521e40c76e35db8421dfe521a9c
Jacqui Lough <lough.jacqui@gmail.com>
2021-06-10 12:59:31 +1000

    test that a section page renders successfully with cypress

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,8 +1,14 @@
-describe('Home', () => {
-  it('renders successfully', () => {
+describe('Page tests', () => {
+  it('renders the homepage successfully', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('Oaklyn')
     cy.get('header nav a[href="/business"]')
     cy.get('a.featured')
+  })
+  it('renders the business section page successfully', () => {
+    cy.visit('http://localhost:3000/business')
+    cy.contains('Oaklyn')
+    cy.get('h3').contains("Business")
+    cy.get('ul li div h4 a')
   })
 })
