8aa0917a66fbb018ce38c428ff05cc1f8161ddf8
Jacqui Lough <lough.jacqui@gmail.com>
2021-06-11 14:22:12 +1000

    spec for the about page

---
+++ cypress/integration/about_spec.js
@@ -0,0 +1,11 @@
+describe('about page', () => {
+  it('renders successfully', () => {
+    cy.visit('http://localhost:3000/about')
+    cy.contains('The Oaklyn Observer')
+    cy.get('h1').contains("About Oaklyn")
+    cy.get('h1 span').contains("About")
+    // has at least one paragraph of text
+    cy.get('div p span')
+    cy.get('h2').contains("Our Staff")
+  })
+})
