2b45a4d67103b5a4976f77e31037b7131097e8e0
colinpaulevans <68857643+colinpaulevans@users.noreply.github.com>
2021-06-21 11:34:37 -0400

    Added Spotify and Apple Podcasts functionality

--- components/nodes/EmbedNode.js
+++ components/nodes/EmbedNode.js
@@ -4,6 +4,8 @@ import Youtube from './embeds/Youtube';
 import TikTokEmbed from './embeds/TikTok';
 import Facebook from './embeds/Facebook';
 import Instagram from './embeds/Instagram';
+import Spotify from './embeds/Spotify';
+import ApplePodcasts from './embeds/ApplePodcasts';
 
 const EmbedWrapper = tw.div`mb-5 max-w-full w-full`;
 
@@ -26,6 +28,12 @@ export default function EmbedNode({ node, amp }) {
       break;
     case 'instagram.com':
       el = <Instagram node={node} />;
+      break;
+    case 'open.spotify.com':
+      el = <Spotify node={node} />;
+      break;
+    case 'embed.podcasts.apple.com':
+      el = <ApplePodcasts node={node} />;
       break;
     default:
       el = (
