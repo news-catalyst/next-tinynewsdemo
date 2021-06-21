ccedab0fb611327d23dac5f5cc74b74a862cafdd
Tyler Fisher <tyler@tylerjfisher.com>
2021-06-21 14:56:32 -0400

    Clear out the instagram/facebook embeds until we get API access

--- components/nodes/embeds/Instagram.js
+++ components/nodes/embeds/Instagram.js
@@ -1,24 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react';
 
 export default function Instagram({ node }) {
-    const [video, setVideo] = useState(null);
-    useEffect(() => {
-        fetch("https://graph.facebook.com/v11.0/instagram_oembed?url=" + node.link)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setVideo(result.html);
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-            )
-      }, [])
-    const el = (
-      <div>
-        <div dangerouslySetInnerHTML={{ __html: video }} />
-      </div>
-    );
-    return el;
-  }
-  
+  const el = <div />;
+  return el;
+}
