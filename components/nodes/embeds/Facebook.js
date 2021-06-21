067f8f26184143650bfa237db30402442fc50f2b
Tyler Fisher <tyler@tylerjfisher.com>
2021-06-21 14:56:32 -0400

    Clear out the instagram/facebook embeds until we get API access

--- components/nodes/embeds/Facebook.js
+++ components/nodes/embeds/Facebook.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { FacebookProvider, EmbeddedPost } from 'react-facebook';
- 
+
 export default function Facebook({ node }) {
-  return (
-    <FacebookProvider appId="123456789">
-      <EmbeddedPost href={node.link} width="500" />
-    </FacebookProvider>
-  );
+  return <div />;
 }
