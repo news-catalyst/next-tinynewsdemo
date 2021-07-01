96af463512a8af54d724043685ef51697f0f1eb0
Tyler Fisher <tyler@tylerjfisher.com>
2021-07-01 17:32:13 -0400

    Final fixes (haha, final)

--- components/curriculum/HomepageHeader.js
+++ components/curriculum/HomepageHeader.js
@@ -1,4 +1,6 @@
 import tw from 'twin.macro';
+import Image from 'next/image';
+import HomepageImage from '../../public/curriculum/homepage.jpg';
 
 const SectionLayout = tw.section`flex mb-24`;
 const SectionContainer = tw.div`px-5 max-w-7xl mx-auto flex flex-row flex-wrap w-full`;
@@ -6,7 +8,7 @@ const Card = tw.div`w-full shadow-lg border-gray-200 border`;
 const CardImageWrapper = tw.div`w-full mb-8`;
 const CardImg = tw.div`w-full h-96 bg-blue-500`;
 const CardTextWrapper = tw.div`w-full px-8 pb-8`;
-const Title = tw.h1`font-bold text-4xl leading-6 text-center w-full mb-8 max-w-2xl mx-auto`;
+const Title = tw.h1`font-bold text-4xl text-center w-full mb-8 mx-auto`;
 const Dek = tw.p`text-xl max-w-2xl mx-auto mb-4`;
 
 export default function HomepageHeader() {
@@ -15,17 +17,18 @@ export default function HomepageHeader() {
       <SectionContainer>
         <Card>
           <CardImageWrapper>
-            <CardImg />
+            <Image src={HomepageImage} alt="Paper airplanes" />
           </CardImageWrapper>
+          <Title>Welcome to the Tiny News Collective Curriculum Site!</Title>
           <CardTextWrapper>
-            <Title>Tiny News Collective Curriculum</Title>
             <Dek>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna.
-            </Dek>
-            <Dek>
-              Augue eget arcu dictum varius duis. Luctus venenatis lectus magna
-              fringilla urna.
+              This site will provide the information and resources you need to
+              navigate the 12-week TNC training course. You’ll find details
+              about the two weekly sessions alongside helpful tutorials on tools
+              and technology. Best of all? This site is published using the very
+              CMS you’ll use in your tech stack, so we’re continually finding
+              ways to make it simpler and more efficient. We’ll be updating it
+              as we go, so be sure to check it often!
             </Dek>
           </CardTextWrapper>
         </Card>
