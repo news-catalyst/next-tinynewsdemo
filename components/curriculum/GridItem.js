8504444559094bd0238067db89bbdc3ab9a306cb
Tyler Fisher <tyler@tylerjfisher.com>
2021-07-01 13:57:20 -0400

    Display images on homepage grid

--- components/curriculum/GridItem.js
+++ components/curriculum/GridItem.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import tw from 'twin.macro';
+import MainImage from '../articles/MainImage';
 
 const ItemWrapper = tw.div`w-full md:w-1/2 lg:w-1/3 p-4`;
 const Item = tw.div`w-full border border-gray-200 shadow-lg`;
-const ItemImageContainer = tw.div`w-full h-48 bg-blue-500 mb-4`;
+const ItemImageContainer = tw.div`w-full h-64 mb-4`;
 const ItemTextContainer = tw.div`w-full p-6 pt-0`;
 const Hammer = tw.p`text-xs font-bold uppercase mb-2 text-gray-500`;
 const ItemHeader = tw.h3`text-2xl font-bold leading-tight tracking-tight mb-4`;
@@ -22,7 +23,9 @@ export default function GridItem({ header, dek, hammer, article }) {
               minHeight: '34rem',
             }}
           >
-            <ItemImageContainer />
+            <ItemImageContainer>
+              <MainImage article={article} />
+            </ItemImageContainer>
             <ItemTextContainer>
               <Hammer>{hammer}</Hammer>
               <ItemHeader>{header}</ItemHeader>
