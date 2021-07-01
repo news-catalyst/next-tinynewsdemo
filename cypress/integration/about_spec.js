f7c9800f485e264af98f954d7a6d5d2eec4365b9
Jacqui Lough <lough.jacqui@gmail.com>
2021-07-01 13:55:15 +1000

    working setup and initial test for cypress tinycms

--- cypress/integration/about_spec.js
+++ cypress/integration/about_spec.js
@@ -1,7 +1,7 @@
 describe('about page', () => {
   it('renders successfully', () => {
     cy.visit('http://localhost:3000/about')
-    cy.contains('The Oaklyn Observer')
+    cy.contains('The Test Org')
     cy.get('h1').contains("About Oaklyn")
     cy.get('h1 span').contains("About")
     // has at least one paragraph of text
