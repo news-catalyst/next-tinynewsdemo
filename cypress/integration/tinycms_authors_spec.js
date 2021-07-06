3f79cf807c55c55a036eef74e23433352803cfef
Jacqui Lough <lough.jacqui@gmail.com>
2021-07-06 14:14:43 +1000

    Added longer timeouts to the assertions that are failing (timing out) in the GH action for authors

--- cypress/integration/tinycms_authors_spec.js
+++ cypress/integration/tinycms_authors_spec.js
@@ -1,36 +1,28 @@
 // import { cypressDeleteAuthors } from "../../lib/authors"
 
 describe('tinycms authors', () => {
+  
   it('renders the list', () => {
     cy.visit('/tinycms/authors')
     cy.get('h1').contains("Authors")
     // has at least one header
     cy.get('table thead tr th')
     cy.get('th').contains("Name")
-
-    cy.get('body')
-      .then(($body) => {
-        // synchronously query from body
-        // to find which element was created
-        if ($body.find('button.delete-author').length) {
-          cy.get('button.delete-author').each(($el, index, $list) => {
-            cy.wrap($el).click()
-            cy.on('window:confirm', () => true);
-          })
-        }
-      })
   })
 
  it('adds a new author', () => {
     cy.visit('/tinycms/authors/add')
+    cy.location('pathname').should('eq', '/tinycms/authors/add')
     cy.get('input[name="name"').type("New AuthorName")
     cy.get('input[name="title"').type("Staff Editor")
     cy.get('input[name="twitter"').type("@twitterHandle")
     // cy.get('input[name="slug"').type("new-author-name")
     cy.get('textarea[name="bio"]').type("New author bio copy tk.")
     cy.get('[type="radio"]').first().check() // Check first radio element - staff
-    cy.get('form').submit()
-    cy.get('strong').contains('Success!')
+    cy.get('form').submit({
+      timeout: 10000
+    })
+    cy.get('strong', { timeout: 10000 }).contains('Success')
   })
 
  it('updates an existing author', () => {
@@ -39,6 +31,6 @@ describe('tinycms authors', () => {
 
     cy.get('input[name="title"').clear().type("Staff Writer")
     cy.get('form').submit()
-    cy.get('strong').contains('Success!')
+    cy.get('strong', { timeout: 10000 }).contains('Success')
   })
 })
