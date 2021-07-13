b881fe250b55bf47a1d92dfceb2f59bbf4f4368d
Tyler Fisher <tyler@tylerjfisher.com>
2021-07-13 14:17:57 -0400

    Update image height

--- components/curriculum/GridItem.js
+++ components/curriculum/GridItem.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const ItemWrapper = tw.div`w-full md:w-1/2 lg:w-1/3 p-4`;
 const Item = tw.div`w-full border border-gray-200 shadow-lg`;
-const ItemImageContainer = tw.div`w-full h-64 mb-4`;
+const ItemImageContainer = tw.div`w-full h-full mb-4`;
 const ItemTextContainer = tw.div`w-full p-6 pt-0`;
 const Hammer = tw.p`text-xs font-bold uppercase mb-2 text-gray-500`;
 const ItemHeader = tw.h3`text-2xl font-bold leading-tight tracking-tight mb-4`;
