2c12cbe32a4504560aab15ba00b889dad71de7a2
Tyler Fisher <tyler@tylerjfisher.com>
2021-07-15 13:40:16 -0400

    Display logo if available

--- components/nav/GlobalNav.js
+++ components/nav/GlobalNav.js
@@ -7,6 +7,7 @@ import Typography from '../common/Typography';
 const NavContainer = tw.header`border-b border-gray-200 flex w-full`;
 const NavInnerContainer = tw.div`lg:p-5 flex flex-wrap flex-row mx-auto max-w-7xl w-full justify-items-start pt-5`;
 const NavHeader = tw.h1`text-4xl leading-none font-bold ml-4 lg:ml-0 flex-1 order-1`;
+const Logo = tw.img`lg:ml-0 flex-1 order-1 h-16 w-auto`;
 const RightNav = tw.nav`lg:text-right lg:flex-1 flex flex-row flex-nowrap mt-5 order-3 lg:order-none overflow-y-hidden w-full flex-grow border-t border-gray-200 lg:border-t-0 lg:w-auto lg:block lg:mt-0`;
 const SectionLink = styled.a(({ meta }) => ({
   ...tw`lg:items-center lg:mr-8 lg:py-0 inline-flex items-end h-full py-2 px-5 lg:pb-0 lg:px-0`,
@@ -27,17 +28,17 @@ export default function GlobalNav({ metadata, sections }) {
   }
 
   let title;
+  let logo;
   if (metadata) {
     title = metadata['shortName'];
+    logo = metadata['logo'];
   }
 
   return (
     <NavContainer>
       <NavInnerContainer>
         <Link href="/">
-          <a>
-            <NavHeader>{title}</NavHeader>
-          </a>
+          <a>{logo ? <Logo src={logo} /> : <NavHeader>{title}</NavHeader>}</a>
         </Link>
         <RightNav>{sectionLinks}</RightNav>
         {process.env.NEXT_PUBLIC_MONKEYPOD_URL && (
