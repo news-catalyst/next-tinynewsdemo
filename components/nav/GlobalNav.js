e8a902f4378375355a73c005803e74f92af31d80
Jacqui Lough <lough.jacqui@gmail.com>
2021-07-28 11:38:30 +1000

    categories update: hide in global nav, but always render category page

--- components/nav/GlobalNav.js
+++ components/nav/GlobalNav.js
@@ -20,13 +20,16 @@ export default function GlobalNav({ metadata, sections, isAmp }) {
   let sectionLinks;
 
   if (sections && sections[0] && typeof sections[0].title === 'string') {
-    sectionLinks = sections.slice(0, 4).map((section) => (
-      <Link key={`navbar-${section.slug}`} href={`/${section.slug}`} passHref>
-        <SectionLink href={`/${section.slug}`} meta={metadata}>
-          {section.title}
-        </SectionLink>
-      </Link>
-    ));
+    sectionLinks = sections
+      .slice(0, 4)
+      .filter((section) => section.published)
+      .map((section) => (
+        <Link key={`navbar-${section.slug}`} href={`/${section.slug}`} passHref>
+          <SectionLink href={`/${section.slug}`} meta={metadata}>
+            {section.title}
+          </SectionLink>
+        </Link>
+      ));
   }
 
   let title;
