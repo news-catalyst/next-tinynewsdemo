59bfec469ce55df79a441b18ba2721e0a47c51b5
Jacqui Lough <lough.jacqui@gmail.com>
2021-08-03 09:58:52 +1000

    styles for article archive pagination links

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -7,6 +7,22 @@ const CustomStyles = createGlobalStyle`
     -webkit-tap-highlight-color: ${theme`colors.purple.500`};
     ${tw`antialiased`}
   }
+  ul.pagination {
+    display: inline-flex;
+  }
+  ul.pagination li {
+    padding-top: .5rem;
+    padding-bottom: .5rem;
+    padding-left: .5rem;
+    padding-right: .5rem;
+    position: relative;
+  }
+  ul.pagination li.disabled {
+    display: none;
+  }
+  ul.pagination li.active {
+    font-weight: bold;
+  }
 `;
 
 const GlobalStyles = () => (
