1fd00f28f1ace655cd4e0f90c25d879da5182a6f
Jacqui Lough <lough.jacqui@gmail.com>
2021-08-19 13:39:38 +1000

    Simple index page for newsletters archive

--- pages/newsletters/index.js
+++ pages/newsletters/index.js
@@ -1,47 +1,32 @@
-import { useRouter } from 'next/router';
 import React from 'react';
+import Link from 'next/link';
 import tw from 'twin.macro';
 import Layout from '../../components/Layout.js';
 import { cachedContents } from '../../lib/cached';
-import {
-  hasuraGetNewsletter,
-  hasuraListNewsletters,
-} from '../../lib/newsletters.js';
+import { hasuraListNewsletters } from '../../lib/newsletters.js';
 import { getArticleAds } from '../../lib/ads.js';
 import {
   ArticleTitle,
   PostTextContainer,
   PostText,
 } from '../../components/common/CommonStyles.js';
-import {
-  hasuraLocaliseText,
-  renderNewsletterContent,
-} from '../../lib/utils.js';
+import { hasuraLocaliseText } from '../../lib/utils.js';
 import { useAmp } from 'next/amp';
 
 const SectionContainer = tw.div`flex flex-col flex-nowrap items-center px-5 mx-auto max-w-7xl w-full`;
 
-export default function NewsletterEditionPage(props) {
+export default function NewsletterIndexPage(props) {
   const isAmp = useAmp();
-  const router = useRouter();
 
-  // If the page is not yet generated, this will be displayed
-  // initially until getStaticProps() finishes running
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-
-  let body;
-
-  if (props.newsletter) {
-    console.log(typeof props.newsletter.content, props.newsletter.content);
-    body = renderNewsletterContent(
-      props.newsletter.content,
-      [],
-      isAmp,
-      props.siteMetadata
+  const newsletterLinks = props.newsletters.map((newsletter, i) => {
+    return (
+      <li key={`newsletter-item-${i}`}>
+        <Link href={`/newsletters/${newsletter.slug}`}>
+          <a href={`/newsletters/${newsletter.slug}`}>{newsletter.headline}</a>
+        </Link>
+      </li>
     );
-  }
+  });
 
   return (
     <Layout
@@ -51,78 +36,44 @@ export default function NewsletterEditionPage(props) {
     >
       <SectionContainer>
         <ArticleTitle meta={props.siteMetadata} tw="text-center">
-          {props.newsletter.headline}
+          Newsletter Archive
         </ArticleTitle>
         <PostText>
-          <PostTextContainer>{body}</PostTextContainer>
+          <PostTextContainer>
+            <ul>{newsletterLinks}</ul>
+          </PostTextContainer>
         </PostText>
       </SectionContainer>
     </Layout>
   );
 }
 
-export async function getStaticPaths({ locales }) {
-  const apiUrl = process.env.HASURA_API_URL;
-  const apiToken = process.env.ORG_SLUG;
-
-  let paths = [];
-  const { errors, data } = await hasuraListNewsletters({
-    url: apiUrl,
-    orgSlug: apiToken,
-    locale_code: locales.first,
-  });
-
-  if (errors || !data) {
-    return {
-      paths,
-      fallback: true,
-    };
-  }
-
-  const siteLocales = process.env.LOCALES.split(',');
-
-  for (const newsletter of data.newsletter_editions) {
-    for (const locale of siteLocales) {
-      paths.push({
-        params: {
-          slug: newsletter.slug,
-        },
-        locale: locale.locale_code,
-      });
-    }
-  }
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
 export async function getStaticProps({ locale, params }) {
   const apiUrl = process.env.HASURA_API_URL;
   const apiToken = process.env.ORG_SLUG;
 
-  let newsletter;
+  let newsletters = [];
   let sections = [];
   let tags = [];
   let siteMetadata;
 
-  const { errors, data } = await hasuraGetNewsletter({
+  const { errors, data } = await hasuraListNewsletters({
     url: apiUrl,
     orgSlug: apiToken,
-    slug: params.slug,
     locale_code: locale,
   });
 
   if (errors || !data) {
-    console.error('error getting newsletter:', errors);
+    console.error(errors);
     return {
       notFound: true,
     };
   } else {
-    newsletter = data.newsletter_editions[0];
+    newsletters = data.newsletter_editions;
     sections = data.categories;
     tags = data.tags;
+
+    sections = data.categories;
 
     for (var i = 0; i < sections.length; i++) {
       sections[i].title = hasuraLocaliseText(
@@ -149,19 +100,13 @@ export async function getStaticProps({ locale, params }) {
     expandedAds = allAds.filter((ad) => ad.adTypeId === 166 && ad.status === 4);
   }
 
-  let renderFooter = true;
-  if (process.env.ORG_SLUG === 'tiny-news-curriculum') {
-    renderFooter = false; // turns off the global footer for the curriculum site
-  }
-
   return {
     props: {
-      newsletter,
+      newsletters,
       tags,
       sections,
       siteMetadata,
       expandedAds,
-      renderFooter,
     },
   };
 }
