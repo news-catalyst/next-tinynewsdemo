3589215007d107ab26bcd81352d90d80f3758a08
Jacqui Lough <lough.jacqui@gmail.com>
2021-08-23 07:35:05 +1000

    removed unused code from newsletter rendering

--- components/nodes/NewsletterTextNode.js
+++
@@ -1,7 +0,0 @@
-import { Paragraph } from '../common/CommonStyles';
-
-export default function NewsletterTextNode({ node }) {
-  let text = node.insert;
-
-  return <Paragraph>{text}</Paragraph>;
-}
