37b958a21fe9bf7725d1efe4a234ed78f57c6e8b
Jacqui Lough <lough.jacqui@gmail.com>
2021-08-26 07:56:34 +1000

    debug: display process.env

--- script/build-newsletter-editions.js
+++ script/build-newsletter-editions.js
@@ -10,8 +10,12 @@ const apiUrl = process.env.HASURA_API_URL;
 const apiToken = process.env.ORG_SLUG;
 
 function getNewsletterEditions() {
+  console.log("ENV:", process.env);
+  if (!process.env.LETTERHEAD_API_URL) {
+    return;
+  }
   const letterheadUrl = process.env.LETTERHEAD_API_URL + "channels/" + process.env.LETTERHEAD_CHANNEL_SLUG + "/letters";
-  console.log("Letterhead API:", letterheadUrl);
+  
   const opts = {
     headers: {
       'Content-Type': 'application/json',
