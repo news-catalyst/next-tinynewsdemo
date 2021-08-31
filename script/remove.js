f0886558bd5be7d28eee003cf87bce161dff3104
Jacqui Lough <lough.jacqui@gmail.com>
2021-08-31 11:52:47 +1000

    yarn remove:org now also removes GA matching profiles for the organisation, requires -n name parameter

--- script/remove.js
+++ script/remove.js
@@ -15,7 +15,15 @@ const adminSecret = process.env.HASURA_ADMIN_SECRET;
 const githubRepo = process.env.GIT_REPO;
 const githubToken = process.env.GITHUB_TOKEN;
 
-async function removeOrganization(slug) {
+
+const { google } = require("googleapis");
+const credentials = require("./credentials.json");
+const scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/analytics", "https://www.googleapis.com/auth/analytics.edit"];
+const auth = new google.auth.JWT(credentials.client_email, null, credentials.private_key, scopes);
+const analytics = google.analytics({version: "v3", auth})
+const googleAnalyticsAccountID = process.env.GA_ACCOUNT_ID;
+
+async function removeOrganization(slug, name) {
   const octokit = new Octokit({
     auth: githubToken,
   });
@@ -38,6 +46,40 @@ async function removeOrganization(slug) {
     console.error(e);
   }
 
+  let statusResponse = await analytics.management.webproperties.list({'accountId': googleAnalyticsAccountID});
+  if (statusResponse.data.items && statusResponse.data.items.length) {
+    let items = statusResponse.data.items;
+    for await (let item of items) {
+      // console.log(`[GA] * ${item.id}: ${item.name} (${item.profileCount} profiles)` )
+      
+      if (name === item.name) {
+        console.log( "[GA] *** Listing profile IDs in property " + item.id + " ***")
+        let response = await analytics.management.profiles.list({
+            accountId: googleAnalyticsAccountID,
+            webPropertyId: item.id
+        })
+        if (response && response.data && response.data.items && response.data.items[0]) {
+          let profileId = response.data.items[0].id;
+
+          let deleteGA = await analytics.management.profiles.delete(
+            {
+              accountId: googleAnalyticsAccountID,
+              webPropertyId: item.id,
+              profileId: profileId
+          });
+        
+          if (deleteGA && deleteGA.statusText === "OK") {
+            console.log("*** [GA] Deleted profile from Google Analytics:", item.id, profileId);
+          } else {
+            console.error(`!!! [GA] Error deleting profile from Google Analytics using webPropertyId:${item.id} and profileId:${profileId}:`, deleteGA);
+          }
+        } else {
+          console.log("*** [GA] skipping - no profiles found for property: " + item.id)
+        }
+
+      }
+    }
+  }
 
   const { errors, data } = await shared.hasuraRemoveOrganization({
     url: apiUrl,
@@ -55,9 +97,10 @@ async function removeOrganization(slug) {
 
 program
   .requiredOption('-s, --slug <slug>', 'unique slug identifier of the organization')
+  .requiredOption('-n, --name <name>', 'name of the organization as found in Google Analytics')
   .description("removes all data for the specified org")
   .action( (opts) => {
-    removeOrganization(opts.slug);
+    removeOrganization(opts.slug, opts.name);
   });
 
 program.parse(process.argv);
