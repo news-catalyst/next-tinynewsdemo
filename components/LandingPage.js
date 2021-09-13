e3917086119119606683d687f662c9623c4b0d4e
Tyler Fisher <tyler@tylerjfisher.com>
2021-09-13 15:50:43 -0400

    Add backup dek if landing page not filled out

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -7,7 +7,7 @@ const Container = tw.div`flex items-center justify-center flex-col min-h-screen 
 const Title = tw.h1`text-6xl font-bold mb-8`;
 const Dek = tw.div`text-xl mb-6`;
 const DekP = tw.p`mb-5 leading-relaxed`;
-const DekA = tw.a`underline text-blue-700`;
+const DekA = tw.a`text-black cursor-pointer border-b border-blue-500`;
 const BlockWrapper = tw.div`w-full`;
 
 export default function LandingPage({ siteMetadata, sections }) {
@@ -28,7 +28,7 @@ export default function LandingPage({ siteMetadata, sections }) {
               },
             }}
           >
-            {siteMetadata.landingPageDek}
+            {siteMetadata.landingPageDek || siteMetadata.aboutDek}
           </Markdown>
         </Dek>
         <BlockWrapper>
