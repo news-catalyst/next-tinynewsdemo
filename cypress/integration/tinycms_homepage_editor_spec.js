d94d5f725cfe675ef1ca1bccf7407928de9aa8a1
Jacqui Lough <lough.jacqui@gmail.com>
2021-09-16 10:55:03 +1000

    fixed homepage editor bug, newly selected layout now gets 'current' label

--- cypress/integration/tinycms_homepage_editor_spec.js
+++ cypress/integration/tinycms_homepage_editor_spec.js
@@ -1,9 +1,9 @@
 describe('tinycms homepage editor', () => {
   it('displays the editor and changes layout', () => {
-    cy.visit('/tinycms/homepage')
-    cy.get('a').contains("Change Layout:")
-    cy.get('a.layout-switcher:last').click()
+    cy.visit('/tinycms/homepage');
+    cy.get('a').contains('Change Layout:');
+    cy.get('a.layout-switcher:last', { timeout: 10000 }).click();
     // cy.get('form').submit()
-    cy.get('a.layout-switcher:last').contains("(current)")
-  })
-})
+    cy.get('a.layout-switcher:last', { timeout: 10000 }).contains('(current)');
+  });
+});
