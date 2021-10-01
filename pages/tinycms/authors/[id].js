7edc2ed73a228f116acbaf34e337938a5ee270d6
Jacqui Lough <lough.jacqui@gmail.com>
2021-10-01 10:58:36 +1000

    Revert "wip trying to get quill to render properly in the tinycms"
    This reverts commit 26cfaba10791237d706b20eaad4808740a2ecd07.

--- pages/tinycms/authors/[id].js
+++ pages/tinycms/authors/[id].js
@@ -20,10 +20,6 @@ import {
   validateAuthorName,
 } from '../../../lib/utils.js';
 
-import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
-import 'react-quill/dist/quill.snow.css';
-
 const UploadContainer = tw.div`container mx-auto min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10`;
 
 export default function EditAuthor({
@@ -199,15 +195,12 @@ export default function EditAuthor({
             onChange={(ev) => setSlug(ev.target.value)}
             label="Slug"
           />
-
-          <ReactQuill theme="snow" value={bio} onChange={setBio} />
-
-          {/* <TinyTextArea
+          <TinyTextArea
             name="bio"
             value={bio}
             onChange={(ev) => setBio(ev.target.value)}
             label="Bio"
-          /> */}
+          />
           <TinyYesNoField
             name="staff"
             value={staffYesNo}
