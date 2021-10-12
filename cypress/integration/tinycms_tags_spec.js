cc5e92af8c95607d7cd49b5395d648cf294b90a0
Jacqui Lough <lough.jacqui@gmail.com>
2021-10-13 10:50:35 +1100

    update tags cypress test keeps failing, weirdly; combining the list + update tests works though, and is probably faster to run

--- cypress/integration/tinycms_tags_spec.js
+++ cypress/integration/tinycms_tags_spec.js
@@ -1,12 +1,20 @@
 // import { cypressDeleteAuthors } from "../../lib/authors"
 
 describe('tinycms tags', () => {
-  it('renders the list', () => {
+  it('renders the list and updates the first tag', () => {
     cy.visit('/tinycms/tags');
     cy.get('h1').contains('Tags');
     // has at least one header
     cy.get('table thead tr th');
     cy.get('th').contains('Name');
+
+    cy.get('table>tbody>tr:first>td>a')
+      .click()
+      .then(() => {
+        cy.get('input[name="title"').clear().type('Updated Tag');
+        cy.get('form').submit();
+        cy.get('strong').contains('Success!');
+      });
   });
 
   it('adds a new tag', () => {
@@ -18,17 +26,6 @@ describe('tinycms tags', () => {
     cy.get('form')
       .submit()
       .then(() => {
-        cy.get('strong').contains('Success!');
-      });
-  });
-
-  it('updates an existing tag', () => {
-    cy.visit('/tinycms/tags');
-    cy.get('table>tbody>tr:first>td>a')
-      .click()
-      .then(() => {
-        cy.get('input[name="title"').clear().type('Updated Tag');
-        cy.get('form').submit();
         cy.get('strong').contains('Success!');
       });
   });
