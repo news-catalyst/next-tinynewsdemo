99e670c1caa512b1bbfe96b1db719550f61b2a81
Jacqui Lough <lough.jacqui@gmail.com>
2021-11-30 15:32:21 -0500

    WIP wordpress WXR generator script

---
+++ script/wordpress.js
@@ -0,0 +1,164 @@
+require('dotenv').config({ path: '.env.local' });
+
+var ReactDOMServer = require('react-dom/server');
+const ArticleBody = require('../components/articles/ArticleBody.js').default;
+const Importer = require('wxr-generator');
+const shared = require('./shared');
+// const utils = require('../lib/utils');
+
+const apiUrl = process.env.HASURA_API_URL;
+const apiToken = process.env.ORG_SLUG;
+
+async function importSite() {
+  let result = await shared.hasuraGetSiteData({
+    url: apiUrl,
+    orgSlug: apiToken,
+  });
+
+  if (result.errors) {
+    console.error('Error getting site data:', result.errors);
+    return;
+  }
+  let articles = result.data.articles;
+  let categories = result.data.categories;
+  let orgLocales = result.data.organization_locales;
+  let siteMetadatas = result.data.site_metadatas;
+
+  orgLocales.forEach((orgLocale) => {
+    let localeCode = orgLocale.locale.code;
+
+    let metadata = siteMetadatas[0].site_metadata_translations.find(
+      (t) => t.locale_code === localeCode
+    );
+
+    if (!metadata) {
+      console.error('Failed finding site metadata for locale ' + localeCode);
+      return;
+    }
+
+    console.log(localeCode, '->metadata->', metadata.data);
+
+    var importer = new Importer({
+      name: metadata.data.shortName,
+      url: metadata.data.siteUrl,
+      description: metadata.data.aboutDek,
+      language: localeCode,
+    });
+
+    categories.forEach((cat) => {
+      let categoryTranslation = cat.category_translations.find(
+        (t) => t.locale_code === localeCode
+      );
+      if (categoryTranslation) {
+        console.log('adding category:', cat.slug, categoryTranslation.title);
+        importer.addCategory({
+          slug: cat.slug,
+          name: categoryTranslation.title,
+        });
+      }
+    });
+
+    articles.forEach((article) => {
+      let articleTranslation = article.article_translations.find(
+        (t) => t.locale_code === localeCode
+      );
+
+      let articleContent;
+
+      // TBD: figure out how to render the article body contents from here
+      //  the following code throws errors:
+
+      // 1. this throws `SyntaxError: Unexpected token '<'` error; not sure how to fix this;
+      //   found some people on SO using React.createElement() but doesn't work for me :(
+      // articleContent = ReactDOMServer.renderToStaticMarkup(
+      // <!-- React.createElement( // not using this extra function, but have tried it
+      //   <ArticleBody
+      //     article={article}
+      //     isAmp={false}
+      //     ads={false}
+      //     metadata={metadata}
+      //     locale={localeCode}
+      //   />
+      //  --> )
+      // );
+
+      // 2. this goes down a can't use import outside a module (vs require) rabbit hole
+      // const utils = require('../lib/utils');
+      // articleContent = utils.renderBody(
+      //   localeCode,
+      //   article.article_translations,
+      //   false,
+      //   false,
+      //   metadata
+      // );
+
+      let articleCategoryTranslation = article.category.category_translations.find(
+        (c) => c.locale_code === localeCode
+      );
+      console.log('category: ' + articleCategoryTranslation.title);
+      let articleCategories = [
+        {
+          slug: article.category.slug,
+          name: articleCategoryTranslation.title,
+        },
+      ];
+      let articleTags = [];
+      article.tag_articles.forEach((tagArticle) => {
+        let tagTranslation = tagArticle.tag.tag_translations.find(
+          (t) => t.locale_code === localeCode
+        );
+        console.log('tag: ' + tagTranslation.title);
+        if (tagTranslation) {
+          articleTags.push({
+            slug: tagArticle.tag.slug,
+            name: tagTranslation.title,
+          });
+        }
+      });
+
+      let articleAuthors = [];
+      article.author_articles.forEach((authorArticle) => {
+        let authorName =
+          authorArticle.author.first_names.join(' ') +
+          ' ' +
+          authorArticle.author.last_name;
+        console.log('author: ' + authorName);
+        importer.addUser({
+          username: authorArticle.author.slug,
+          display_name: authorName,
+          first_name: authorArticle.author.first_names,
+          last_name: authorArticle.author.last_name,
+        });
+        articleAuthors.push(authorName);
+      });
+      let articleUrl = `/articles/${article.category.slug}/${article.slug}`;
+      if (articleTranslation) {
+        console.log('article: ' + articleTranslation.title);
+        importer.addPost({
+          id: article.id,
+          title: articleTranslation.title,
+          url: articleUrl,
+          slug: article.slug,
+          date: articleTranslation.first_published_at,
+          author: articleAuthors.join(', '),
+          content: articleContent,
+          summary: articleTranslation.search_description,
+          comment_status: 'open',
+          ping_status: 'closed',
+          categories: articleCategories,
+          tags: articleTags,
+        });
+      }
+    });
+
+    let wxrContent = importer.stringify();
+    console.log();
+    console.log(wxrContent);
+  });
+}
+
+async function main() {
+  importSite();
+}
+
+main().catch((error) => console.error(error));
