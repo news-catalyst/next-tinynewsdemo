c2969e0b8159413bca6aecbbefb55dbb15166845
Jacqui Lough <lough.jacqui@gmail.com>
2021-12-02 11:12:04 -0500

    fixed issues with awaiting content and order of importer tasks; wxr is now generated only after all articles/posts are retrieved

--- script/wordpress.js
+++ script/wordpress.js
@@ -6,6 +6,95 @@ const shared = require('./shared');
 const apiUrl = process.env.HASURA_API_URL;
 const apiToken = process.env.ORG_SLUG;
 
+async function processArticles(importer, localeCode, articles) {
+  for (const article of articles) {
+    let articleTranslation = article.article_translations.find(
+      (t) => t.locale_code === localeCode
+    );
+
+    if (!articleTranslation) {
+      console.error(
+        ' ! ' +
+          article.slug +
+          ' failed finding translation in ' +
+          localeCode +
+          ', skipping'
+      );
+      continue;
+    }
+    let articleContent;
+
+    let articleCategoryTranslation = article.category.category_translations.find(
+      (c) => c.locale_code === localeCode
+    );
+
+    let articleCategories = [
+      {
+        slug: article.category.slug,
+        name: articleCategoryTranslation.title,
+      },
+    ];
+    let articleTags = [];
+    if (article.tag_articles) {
+      article.tag_articles.forEach((tagArticle) => {
+        let tagTranslation = tagArticle.tag.tag_translations.find(
+          (t) => t.locale_code === localeCode
+        );
+        // console.log('tag: ' + tagTranslation.title);
+        if (tagTranslation) {
+          articleTags.push({
+            slug: tagArticle.tag.slug,
+            name: tagTranslation.title,
+          });
+        }
+      });
+    }
+
+    let articleAuthors = [];
+    article.author_articles.forEach((authorArticle) => {
+      let authorName =
+        authorArticle.author.first_names + ' ' + authorArticle.author.last_name;
+      // console.log('author: ' + authorName);
+      importer.addUser({
+        username: authorArticle.author.slug,
+        display_name: authorName,
+        first_name: authorArticle.author.first_names,
+        last_name: authorArticle.author.last_name,
+      });
+      articleAuthors.push(authorName);
+    });
+    let articleUrl = `/articles/${article.category.slug}/${article.slug}`;
+    let apiArticleUrl = `http://localhost:3000/api${articleUrl}?secret=${process.env.PREVIEW_TOKEN}`;
+
+    console.log(' - retrieving article content from: ' + apiArticleUrl);
+    const articleResult = await fetch(apiArticleUrl);
+    const data = await articleResult.json();
+
+    // console.log('data.content:', data.content);
+    articleContent = data.content;
+
+    if (articleTranslation) {
+      console.log(
+        ' - including article as post: ' + articleTranslation.headline
+      );
+      importer.addPost({
+        id: article.id,
+        title: articleTranslation.headline,
+        url: articleUrl,
+        slug: article.slug,
+        date: articleTranslation.first_published_at,
+        author: articleAuthors.join(', '),
+        content: articleContent,
+        summary: articleTranslation.search_description,
+        comment_status: 'open',
+        ping_status: 'closed',
+        categories: articleCategories,
+        tags: articleTags,
+      });
+    }
+  }
+}
+
 async function importSite() {
   let result = await shared.hasuraGetSiteData({
     url: apiUrl,
@@ -21,7 +110,7 @@ async function importSite() {
   let orgLocales = result.data.organization_locales;
   let siteMetadatas = result.data.site_metadatas;
 
-  orgLocales.forEach((orgLocale) => {
+  for (const orgLocale of orgLocales) {
     let localeCode = orgLocale.locale.code;
 
     let metadata = siteMetadatas[0].site_metadata_translations.find(
@@ -33,7 +122,7 @@ async function importSite() {
       return;
     }
 
-    console.log(localeCode, '->metadata->', metadata.data.keys);
+    console.log('Generating wordpress WXR file for locale ' + localeCode);
 
     var importer = new Importer({
       name: metadata.data.shortName,
@@ -47,7 +136,11 @@ async function importSite() {
         (t) => t.locale_code === localeCode
       );
       if (categoryTranslation) {
-        console.log('adding category:', cat.slug, categoryTranslation.title);
+        console.log(
+          ' - including site section:',
+          cat.slug,
+          categoryTranslation.title
+        );
         importer.addCategory({
           slug: cat.slug,
           name: categoryTranslation.title,
@@ -55,89 +148,12 @@ async function importSite() {
       }
     });
 
-    articles.forEach((article) => {
-      let articleTranslation = article.article_translations.find(
-        (t) => t.locale_code === localeCode
-      );
-
-      let articleContent;
-
-      let articleCategoryTranslation = article.category.category_translations.find(
-        (c) => c.locale_code === localeCode
-      );
-      console.log('category: ' + articleCategoryTranslation.title);
-      let articleCategories = [
-        {
-          slug: article.category.slug,
-          name: articleCategoryTranslation.title,
-        },
-      ];
-      let articleTags = [];
-      if (article.tag_articles) {
-        article.tag_articles.forEach((tagArticle) => {
-          let tagTranslation = tagArticle.tag.tag_translations.find(
-            (t) => t.locale_code === localeCode
-          );
-          console.log('tag: ' + tagTranslation.title);
-          if (tagTranslation) {
-            articleTags.push({
-              slug: tagArticle.tag.slug,
-              name: tagTranslation.title,
-            });
-          }
-        });
-      }
-
-      let articleAuthors = [];
-      article.author_articles.forEach((authorArticle) => {
-        let authorName =
-          authorArticle.author.first_names +
-          ' ' +
-          authorArticle.author.last_name;
-        console.log('author: ' + authorName);
-        importer.addUser({
-          username: authorArticle.author.slug,
-          display_name: authorName,
-          first_name: authorArticle.author.first_names,
-          last_name: authorArticle.author.last_name,
-        });
-        articleAuthors.push(authorName);
-      });
-      let articleUrl = `/articles/${article.category.slug}/${article.slug}`;
-      let apiArticleUrl = `http://localhost:3000/api${articleUrl}?secret=${process.env.PREVIEW_TOKEN}`;
-
-      console.log(apiArticleUrl);
-      fetch(apiArticleUrl)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log('data.content:', data.content);
-          articleContent = data.content;
-
-          if (articleTranslation) {
-            console.log('article title: ' + articleTranslation.headline);
-            importer.addPost({
-              id: article.id,
-              title: articleTranslation.headline,
-              url: articleUrl,
-              slug: article.slug,
-              date: articleTranslation.first_published_at,
-              author: articleAuthors.join(', '),
-              content: articleContent,
-              summary: articleTranslation.search_description,
-              comment_status: 'open',
-              ping_status: 'closed',
-              categories: articleCategories,
-              tags: articleTags,
-            });
-          }
-        })
-        .catch(console.error);
-    });
+    await processArticles(importer, localeCode, articles);
 
     let wxrContent = importer.stringify();
     console.log();
     console.log(wxrContent);
-  });
+  }
 }
 
 async function main() {
