6d699bd4ee0b866ba45f56100bd53e56441e9d66
Jacqui Lough <lough.jacqui@gmail.com>
2021-12-08 10:12:00 -0500

    Use article publish date to determine image paths for wordpress export

--- components/nodes/ImageNodeWordPress.js
+++ components/nodes/ImageNodeWordPress.js
@@ -1,21 +1,29 @@
 var url = require('url');
 var path = require('path');
 
-export default function ImageNodeWordPress({ node }) {
+export default function ImageNodeWordPress({ node, translations, locale }) {
   const image = node.children.find((child) => child.imageUrl);
 
   if (!image) {
     return null;
   }
 
+  let matchingTranslation = translations.find((t) => t.locale_code === locale);
+  if (!matchingTranslation) {
+    matchingTranslation = translations[0];
+  }
+
+  // console.log('first_published_at:', matchingTranslation.first_published_at);
+
   let parsedUrl = url.parse(image.imageUrl);
   let filename = path.basename(parsedUrl.pathname);
 
-  let now = new Date();
-  let year = now.getFullYear();
-  let month = now.getMonth();
-
+  let publishedDate = new Date(matchingTranslation.first_published_at);
+  let year = publishedDate.getFullYear();
+  let month = ('0' + (publishedDate.getMonth() + 1)).slice(-2);
   let wordpressImagePath = `/wp-content/uploads/${year}/${month}/${filename}`;
+
+  // console.log(image.imageUrl, '->', wordpressImagePath);
 
   const figure = (
     <figure key={wordpressImagePath}>
