ab70daaf6cc537c5c17322a7c372d01c83d41209
Jacqui Lough <lough.jacqui@gmail.com>
2021-12-16 12:59:19 -0500

    Added specs for lookups on static pages and new documents

--- cypress/integration/documents_api_lookup_spec.js
+++ cypress/integration/documents_api_lookup_spec.js
@@ -39,4 +39,60 @@ describe('document API', () => {
       );
     });
   });
+
+  it('finds a page', () => {
+    cy.request({
+      url: `/api/sidebar/documents/1cS3u5bdBP7sg29t-nBW8UgvUHDNpiZRFccZA53A04sU?token=${Cypress.env(
+        'API_TOKEN'
+      )}`,
+    }).should((response) => {
+      // should determine if this is an article or a page
+      expect(response.body).to.have.property('documentType', 'page');
+
+      // should return site-wide data for the sidebar form select menus
+      expect(response.body.authors).to.have.length.of.at.least(1);
+      expect(response.body.categories).to.have.length.of.at.least(1);
+      expect(response.body.organization_locales).to.have.length.of.at.least(1);
+      expect(response.body.tags).to.have.length.of.at.least(1);
+      expect(response.body.homepage_layout_datas).to.have.length.of.at.least(1);
+      expect(response.body.page_google_documents).to.have.length.of.at.least(1);
+      expect(response.body.editorUrl).not.to.be.empty;
+
+      // should return article-specific data
+      // expect(response.body.article).to.be.instanceOf(Object);
+      expect(response.body.page).not.to.be.empty;
+      expect(response.body.page).to.have.property('slug', 'test-page-1');
+      expect(response.body.page.page_translations).to.have.length(1);
+      expect(response.body.page.page_translations[0]).to.have.any.keys(
+        'headline',
+        'id',
+        'published',
+        'search_title',
+        'search_description',
+        'locale_code'
+      );
+    });
+  });
+
+  it('responds with sitewide data for an unknown (new) document', () => {
+    cy.request({
+      url: `/api/sidebar/documents/123456abcnotfound?token=${Cypress.env(
+        'API_TOKEN'
+      )}`,
+    }).should((response) => {
+      // should determine if this is an article or a page
+      expect(response.body.documentType).to.be.null;
+
+      // should return site-wide data for the sidebar form select menus
+      expect(response.body.authors).to.have.length.of.at.least(1);
+      expect(response.body.categories).to.have.length.of.at.least(1);
+      expect(response.body.organization_locales).to.have.length.of.at.least(1);
+      expect(response.body.tags).to.have.length.of.at.least(1);
+      expect(response.body.homepage_layout_datas).to.have.length.of.at.least(1);
+      expect(response.body.editorUrl).not.to.be.empty;
+
+      expect(response.body.article).to.be.undefined;
+      expect(response.body.page).to.be.undefined;
+    });
+  });
 });
