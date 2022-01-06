eeb6e0e276e5e8f907d668653fb7329f27f4227a
Jacqui Lough <lough.jacqui@gmail.com>
2022-01-06 12:11:37 -0500

    Setup and teardown static test page data for specs

--- cypress/integration/documents_api_lookup_spec.js
+++ cypress/integration/documents_api_lookup_spec.js
@@ -2,11 +2,9 @@ describe('document API', () => {
   before(() => {
     cy.log('before tests, setting up data...');
     cy.task('db:seed');
-    // cy.fixture('organization').as('usersJson') // load data from users.json
   });
 
   it('finds an article', () => {
-    cy.log(`API TOKEN: ${Cypress.env('apiToken')}`);
     cy.request({
       url: `/api/sidebar/documents/1LSyMzR1KxyKoml6q56DYQaxEV8Qm4EZo2y_xEFIkvGw?token=${Cypress.env(
         'apiToken'
@@ -44,39 +42,36 @@ describe('document API', () => {
     });
   });
 
-  // it('finds a page', () => {
-  //   cy.request({
-  //     url: `/api/sidebar/documents/1cS3u5bdBP7sg29t-nBW8UgvUHDNpiZRFccZA53A04sU?token=${Cypress.env(
-  //       'apiToken'
-  //     )}`,
-  //   }).should((response) => {
-  //     // should determine if this is an article or a page
-  //     expect(response.body).to.have.property('documentType', 'page');
+  it('finds a page', () => {
+    cy.request({
+      url: `/api/sidebar/documents/1cS3u5bdBP7sg29t-nBW8UgvUHDNpiZRFccZA53A04sU?token=${Cypress.env(
+        'apiToken'
+      )}`,
+    }).should((response) => {
+      // should determine if this is an article or a page
+      expect(response.body).to.have.property('documentType', 'page');
 
-  //     // should return site-wide data for the sidebar form select menus
-  //     expect(response.body.authors).to.have.length.of.at.least(1);
-  //     expect(response.body.categories).to.have.length.of.at.least(1);
-  //     expect(response.body.organization_locales).to.have.length.of.at.least(1);
-  //     expect(response.body.tags).to.have.length.of.at.least(1);
-  //     expect(response.body.homepage_layout_datas).to.have.length.of.at.least(1);
-  //     expect(response.body.page_google_documents).to.have.length.of.at.least(1);
-  //     expect(response.body.editorUrl).not.to.be.empty;
+      //     // should return site-wide data for the sidebar form select menus
+      expect(response.body.authors).to.have.length.of.at.least(1);
+      expect(response.body.categories).to.have.length.of.at.least(1);
+      expect(response.body.organization_locales).to.have.length.of.at.least(1);
+      expect(response.body.tags).to.have.length.of.at.least(1);
+      expect(response.body.page_google_documents).to.have.length.of.at.least(1);
+      expect(response.body.editorUrl).not.to.be.empty;
 
-  //     // should return article-specific data
-  //     // expect(response.body.article).to.be.instanceOf(Object);
-  //     expect(response.body.page).not.to.be.empty;
-  //     expect(response.body.page).to.have.property('slug', 'test-page-1');
-  //     expect(response.body.page.page_translations).to.have.length(1);
-  //     expect(response.body.page.page_translations[0]).to.have.any.keys(
-  //       'headline',
-  //       'id',
-  //       'published',
-  //       'search_title',
-  //       'search_description',
-  //       'locale_code'
-  //     );
-  //   });
-  // });
+      expect(response.body.page).not.to.be.empty;
+      expect(response.body.page).to.have.property('slug', 'test-about-page');
+      expect(response.body.page.page_translations).to.have.length(1);
+      expect(response.body.page.page_translations[0]).to.have.any.keys(
+        'headline',
+        'id',
+        'published',
+        'search_title',
+        'search_description',
+        'locale_code'
+      );
+    });
+  });
 
   it('responds with sitewide data for an unknown (new) document', () => {
     cy.request({
