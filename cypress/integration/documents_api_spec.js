1259813cacac2d96a9c9b5a3b8cbd133092b540f
Jacqui Lough <lough.jacqui@gmail.com>
2022-01-06 14:31:50 -0500

    specs covering preview article and page in document api

--- cypress/integration/documents_api_spec.js
+++ cypress/integration/documents_api_spec.js
@@ -1,3 +1,8 @@
+let articleID;
+let categoryID;
+let pageID;
+const faker = require('faker');
+
 describe('document API', () => {
   before(() => {
     cy.log('before tests, setting up data...');
@@ -28,7 +33,10 @@ describe('document API', () => {
         'slug',
         'test-doc-for-article-features'
       );
+
+      articleID = response.body.article.id;
       expect(response.body.article.category).to.have.property('slug', 'news');
+      categoryID = response.body.article.category.id;
       expect(response.body.article.author_articles).not.to.be.empty;
       expect(response.body.article.tag_articles).to.have.length(1);
       expect(response.body.article.article_translations).to.have.length(1);
@@ -61,6 +69,7 @@ describe('document API', () => {
 
       expect(response.body.page).not.to.be.empty;
       expect(response.body.page).to.have.property('slug', 'test-about-page');
+      pageID = response.body.page.id;
       expect(response.body.page.page_translations).to.have.length(1);
       expect(response.body.page.page_translations[0]).to.have.any.keys(
         'headline',
@@ -91,6 +100,116 @@ describe('document API', () => {
 
       expect(response.body.article).to.be.undefined;
       expect(response.body.page).to.be.undefined;
+    });
+  });
+
+  it('previews an article', () => {
+    let articleData = {
+      id: articleID,
+      slug: 'test-doc-for-article-features',
+      category_id: categoryID,
+      article_sources: [],
+      document_id: '1LSyMzR1KxyKoml6q56DYQaxEV8Qm4EZo2y_xEFIkvGw',
+      locale_code: 'en-US',
+      headline: faker.lorem.sentence(),
+      published: true,
+      search_description: faker.lorem.paragraph(),
+      search_title: faker.lorem.sentence(),
+      twitter_title: faker.lorem.sentence(),
+      twitter_description: faker.lorem.paragraph(),
+      facebook_title: faker.lorem.sentence(),
+      facebook_description: faker.lorem.paragraph(),
+    };
+    cy.request(
+      'POST',
+      `/api/sidebar/documents/1LSyMzR1KxyKoml6q56DYQaxEV8Qm4EZo2y_xEFIkvGw/preview?documentType=article&token=${Cypress.env(
+        'apiToken'
+      )}`,
+      {
+        articleData: articleData,
+        contents: [
+          {
+            paragraph: {
+              elements: [
+                {
+                  textRun: {
+                    content: faker.lorem.sentence(),
+                    textStyle: {},
+                  },
+                  startIndex: 1,
+                  endIndex: 22,
+                },
+              ],
+              paragraphStyle: {
+                namedStyleType: 'HEADING_1',
+                direction: 'LEFT_TO_RIGHT',
+                headingId: 'h.3rjhe4n2n39e',
+              },
+            },
+            endIndex: 22,
+            startIndex: 1,
+          },
+        ],
+        slug: 'test-doc-for-article-features',
+        googleAuthToken: faker.lorem.word(),
+      }
+    ).then((response) => {
+      expect(response.body).to.have.property('documentType', 'article');
+      expect(response.body).to.have.property('status', 'success');
+    });
+  });
+
+  it('previews a page', () => {
+    let pageData = {
+      id: pageID,
+      slug: 'test-about-page',
+      document_id: '1cS3u5bdBP7sg29t-nBW8UgvUHDNpiZRFccZA53A04sU',
+      locale_code: 'en-US',
+      headline: faker.lorem.sentence(),
+      published: true,
+      search_description: faker.lorem.paragraph(),
+      search_title: faker.lorem.sentence(),
+      twitter_title: faker.lorem.sentence(),
+      twitter_description: faker.lorem.paragraph(),
+      facebook_title: faker.lorem.sentence(),
+      facebook_description: faker.lorem.paragraph(),
+    };
+    cy.request(
+      'POST',
+      `/api/sidebar/documents/1cS3u5bdBP7sg29t-nBW8UgvUHDNpiZRFccZA53A04sU/preview?documentType=page&token=${Cypress.env(
+        'apiToken'
+      )}`,
+      {
+        pageData: pageData,
+        contents: [
+          {
+            paragraph: {
+              elements: [
+                {
+                  textRun: {
+                    content: faker.lorem.sentence(),
+                    textStyle: {},
+                  },
+                  startIndex: 1,
+                  endIndex: 22,
+                },
+              ],
+              paragraphStyle: {
+                namedStyleType: 'HEADING_1',
+                direction: 'LEFT_TO_RIGHT',
+                headingId: 'h.3rjhe4n2n39e',
+              },
+            },
+            endIndex: 22,
+            startIndex: 1,
+          },
+        ],
+        slug: 'test-about-page',
+        googleAuthToken: faker.lorem.word(),
+      }
+    ).then((response) => {
+      expect(response.body).to.have.property('documentType', 'page');
+      expect(response.body).to.have.property('status', 'success');
     });
   });
 });
