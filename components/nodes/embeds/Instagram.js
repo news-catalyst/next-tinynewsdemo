fae6ff03b443c8af45809261ad99fbb0f855956e
Tyler Fisher <tyler@tylerjfisher.com>
2022-01-11 14:25:01 -0500

    Implement Instagram embeds

--- components/nodes/embeds/Instagram.js
+++ components/nodes/embeds/Instagram.js
@@ -1,5 +1,15 @@
-import { useState, useEffect } from 'react';
+import InstagramEmbed from 'react-instagram-embed';
+
+const FB_APP_ID = '200171875317911';
+const FB_CLIENT_TOKEN = 'c2953b8f7d46b6793b74c2e1ce6eda66';
 
 export default function Instagram({ node }) {
-  return <div />;
+  return (
+    <InstagramEmbed
+      url={node.link}
+      clientAccessToken={`${FB_APP_ID}|${FB_CLIENT_TOKEN}`}
+      hideCaption={false}
+      containerTagName="div"
+    />
+  );
 }
