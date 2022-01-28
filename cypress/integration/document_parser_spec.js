ab9111ffb7e885ab72f6ce9bc6b60b0fb041a516
Jacqui Lough <lough.jacqui@gmail.com>
2022-01-28 09:42:09 -0500

    wip: image upload stubbing and intercepting with cypress

--- cypress/integration/document_parser_spec.js
+++ cypress/integration/document_parser_spec.js
@@ -7,7 +7,7 @@ describe('document API', () => {
 
   let slug = 'foo';
   let oauthToken = 'foobar';
-  const elements = [
+  let elements = [
     {
       sectionBreak: {
         sectionStyle: {
@@ -456,6 +456,21 @@ describe('document API', () => {
         {
           https: true,
           method: 'GET',
+          hostname: 'lh6.googleusercontent.com',
+          url: '/*',
+        },
+        (req) => {
+          req.reply({
+            statusCode: 200,
+            fixture: 'upload.png',
+          });
+        }
+      ).as('googleDocsImage');
+
+      cy.intercept(
+        {
+          https: true,
+          method: 'GET',
           hostname: 'forms.gle',
           url: '/*',
         },
@@ -605,6 +620,524 @@ describe('document API', () => {
         expect(el.link).to.eq(
           'https://docs.google.com/forms/d/e/1FAIpQLSfVOl8qdT54E2r_T367YsRlka57bUY_fnjedMLezp1Tll9OQw/viewform?usp=send_form'
         );
+      });
+    });
+
+    it('formats image nodes', () => {
+      elements = [
+        {
+          endIndex: 1,
+          sectionBreak: {
+            sectionStyle: {
+              sectionType: 'CONTINUOUS',
+              contentDirection: 'LEFT_TO_RIGHT',
+              columnSeparatorStyle: 'NONE',
+            },
+          },
+        },
+        {
+          endIndex: 60,
+          paragraph: {
+            elements: [
+              {
+                textRun: {
+                  content: 'This sentence has a word in ',
+                  textStyle: {},
+                },
+                endIndex: 29,
+                startIndex: 1,
+              },
+              {
+                endIndex: 33,
+                textRun: {
+                  textStyle: {
+                    bold: true,
+                  },
+                  content: 'bold',
+                },
+                startIndex: 29,
+              },
+              {
+                startIndex: 33,
+                textRun: {
+                  textStyle: {},
+                  content: ', ',
+                },
+                endIndex: 35,
+              },
+              {
+                endIndex: 41,
+                textRun: {
+                  textStyle: {
+                    italic: true,
+                  },
+                  content: 'italic',
+                },
+                startIndex: 35,
+              },
+              {
+                startIndex: 41,
+                textRun: {
+                  textStyle: {},
+                  content: ', and ',
+                },
+                endIndex: 47,
+              },
+              {
+                textRun: {
+                  textStyle: {
+                    underline: true,
+                  },
+                  content: 'underlined',
+                },
+                endIndex: 57,
+                startIndex: 47,
+              },
+              {
+                textRun: {
+                  content: '. \n',
+                  textStyle: {},
+                },
+                startIndex: 57,
+                endIndex: 60,
+              },
+            ],
+            paragraphStyle: {
+              namedStyleType: 'NORMAL_TEXT',
+              direction: 'LEFT_TO_RIGHT',
+            },
+          },
+          startIndex: 1,
+        },
+        {
+          startIndex: 60,
+          endIndex: 61,
+          paragraph: {
+            elements: [
+              {
+                endIndex: 61,
+                textRun: {
+                  content: '\n',
+                  textStyle: {},
+                },
+                startIndex: 60,
+              },
+            ],
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              namedStyleType: 'NORMAL_TEXT',
+            },
+          },
+        },
+        {
+          startIndex: 61,
+          endIndex: 137,
+          paragraph: {
+            elements: [
+              {
+                endIndex: 88,
+                textRun: {
+                  content: 'This sentence has a single ',
+                  textStyle: {},
+                },
+                startIndex: 61,
+              },
+              {
+                endIndex: 92,
+                textRun: {
+                  textStyle: {
+                    underline: true,
+                    foregroundColor: {
+                      color: {
+                        rgbColor: {
+                          blue: 0.8,
+                          red: 0.06666667,
+                          green: 0.33333334,
+                        },
+                      },
+                    },
+                    link: {
+                      url: 'https://tinynewsco.org',
+                    },
+                  },
+                  content: 'word',
+                },
+                startIndex: 88,
+              },
+              {
+                endIndex: 137,
+                textRun: {
+                  content: ' linked to the tinynewsco website homepage. \n',
+                  textStyle: {},
+                },
+                startIndex: 92,
+              },
+            ],
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              namedStyleType: 'NORMAL_TEXT',
+            },
+          },
+        },
+        {
+          startIndex: 137,
+          endIndex: 138,
+          paragraph: {
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              namedStyleType: 'NORMAL_TEXT',
+            },
+            elements: [
+              {
+                textRun: {
+                  content: '\n',
+                  textStyle: {},
+                },
+                startIndex: 137,
+                endIndex: 138,
+              },
+            ],
+          },
+        },
+        {
+          paragraph: {
+            elements: [
+              {
+                textRun: {
+                  content: 'list item 1\n',
+                  textStyle: {},
+                },
+                endIndex: 150,
+                startIndex: 138,
+              },
+            ],
+            paragraphStyle: {
+              indentStart: {
+                magnitude: 36,
+                unit: 'PT',
+              },
+              direction: 'LEFT_TO_RIGHT',
+              indentFirstLine: {
+                unit: 'PT',
+                magnitude: 18,
+              },
+              namedStyleType: 'NORMAL_TEXT',
+            },
+            bullet: {
+              textStyle: {
+                underline: false,
+              },
+              listId: 'kix.qqyukljg8mnr',
+            },
+          },
+          startIndex: 138,
+          endIndex: 150,
+        },
+        {
+          paragraph: {
+            elements: [
+              {
+                endIndex: 162,
+                startIndex: 150,
+                textRun: {
+                  content: 'list item 2\n',
+                  textStyle: {},
+                },
+              },
+            ],
+            bullet: {
+              listId: 'kix.qqyukljg8mnr',
+              textStyle: {
+                underline: false,
+              },
+            },
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              indentStart: {
+                magnitude: 36,
+                unit: 'PT',
+              },
+              indentFirstLine: {
+                magnitude: 18,
+                unit: 'PT',
+              },
+              namedStyleType: 'NORMAL_TEXT',
+            },
+          },
+          endIndex: 162,
+          startIndex: 150,
+        },
+        {
+          endIndex: 177,
+          startIndex: 162,
+          paragraph: {
+            elements: [
+              {
+                textRun: {
+                  content: 'Heading Size 1\n',
+                  textStyle: {},
+                },
+                endIndex: 177,
+                startIndex: 162,
+              },
+            ],
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              headingId: 'h.t40hwwqf2an0',
+              namedStyleType: 'HEADING_1',
+            },
+          },
+        },
+        {
+          startIndex: 177,
+          endIndex: 187,
+          paragraph: {
+            paragraphStyle: {
+              namedStyleType: 'NORMAL_TEXT',
+              direction: 'LEFT_TO_RIGHT',
+            },
+            elements: [
+              {
+                endIndex: 187,
+                textRun: {
+                  content: 'paragraph\n',
+                  textStyle: {},
+                },
+                startIndex: 177,
+              },
+            ],
+          },
+        },
+        {
+          endIndex: 202,
+          paragraph: {
+            elements: [
+              {
+                startIndex: 187,
+                endIndex: 202,
+                textRun: {
+                  textStyle: {},
+                  content: 'Heading Size 2\n',
+                },
+              },
+            ],
+            paragraphStyle: {
+              namedStyleType: 'HEADING_2',
+              direction: 'LEFT_TO_RIGHT',
+              headingId: 'h.zdu1t9xjbq6s',
+            },
+          },
+          startIndex: 187,
+        },
+        {
+          endIndex: 212,
+          startIndex: 202,
+          paragraph: {
+            elements: [
+              {
+                endIndex: 212,
+                textRun: {
+                  textStyle: {},
+                  content: 'paragraph\n',
+                },
+                startIndex: 202,
+              },
+            ],
+            paragraphStyle: {
+              namedStyleType: 'NORMAL_TEXT',
+              direction: 'LEFT_TO_RIGHT',
+            },
+          },
+        },
+        {
+          endIndex: 213,
+          startIndex: 212,
+          paragraph: {
+            elements: [
+              {
+                textRun: {
+                  textStyle: {},
+                  content: '\n',
+                },
+                startIndex: 212,
+                endIndex: 213,
+              },
+            ],
+            paragraphStyle: {
+              namedStyleType: 'NORMAL_TEXT',
+              direction: 'LEFT_TO_RIGHT',
+            },
+          },
+        },
+        {
+          startIndex: 213,
+          endIndex: 249,
+          paragraph: {
+            elements: [
+              {
+                startIndex: 213,
+                textRun: {
+                  content: 'https://forms.gle/413sboQBCkw4p1PW6',
+                  textStyle: {
+                    foregroundColor: {
+                      color: {
+                        rgbColor: {
+                          green: 0.33333334,
+                          blue: 0.8,
+                          red: 0.06666667,
+                        },
+                      },
+                    },
+                    link: {
+                      url: 'https://forms.gle/413sboQBCkw4p1PW6',
+                    },
+                    underline: true,
+                  },
+                },
+                endIndex: 248,
+              },
+              {
+                endIndex: 249,
+                textRun: {
+                  textStyle: {},
+                  content: '\n',
+                },
+                startIndex: 248,
+              },
+            ],
+            paragraphStyle: {
+              namedStyleType: 'NORMAL_TEXT',
+              direction: 'LEFT_TO_RIGHT',
+            },
+          },
+        },
+        {
+          endIndex: 250,
+          paragraph: {
+            paragraphStyle: {
+              namedStyleType: 'NORMAL_TEXT',
+              direction: 'LEFT_TO_RIGHT',
+            },
+            elements: [
+              {
+                startIndex: 249,
+                endIndex: 250,
+                textRun: {
+                  textStyle: {},
+                  content: '\n',
+                },
+              },
+            ],
+          },
+          startIndex: 249,
+        },
+        {
+          startIndex: 250,
+          endIndex: 252,
+          paragraph: {
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              namedStyleType: 'NORMAL_TEXT',
+            },
+            elements: [
+              {
+                startIndex: 250,
+                inlineObjectElement: {
+                  textStyle: {},
+                  inlineObjectId: 'kix.al6se8yfh2hr',
+                },
+                endIndex: 251,
+              },
+              {
+                textRun: {
+                  content: '\n',
+                  textStyle: {},
+                },
+                startIndex: 251,
+                endIndex: 252,
+              },
+            ],
+          },
+        },
+        {
+          endIndex: 254,
+          paragraph: {
+            elements: [
+              {
+                inlineObjectElement: {
+                  textStyle: {},
+                  inlineObjectId: 'kix.wr6s7brrno1m',
+                },
+                endIndex: 253,
+                startIndex: 252,
+              },
+              {
+                endIndex: 254,
+                startIndex: 253,
+                textRun: {
+                  textStyle: {},
+                  content: '\n',
+                },
+              },
+            ],
+            paragraphStyle: {
+              direction: 'LEFT_TO_RIGHT',
+              namedStyleType: 'NORMAL_TEXT',
+            },
+          },
+          startIndex: 252,
+        },
+      ];
+      listInfo = { 'kix.qqyukljg8mnr': 'BULLET' };
+      imageList = {
+        'kix.wr6s7brrno1m':
+          'https://assets.tinynewsco.org/oaklyn/cypress-test-doc/imagekix.wr6s7brrno1m.png',
+      };
+      inlineObjects = {
+        'kix.wr6s7brrno1m': {
+          inlineObjectProperties: {
+            embeddedObject: {
+              marginTop: { magnitude: 9, unit: 'PT' },
+              size: {
+                height: { unit: 'PT', magnitude: 565 },
+                width: { unit: 'PT', magnitude: 468 },
+              },
+              marginRight: { magnitude: 9, unit: 'PT' },
+              embeddedObjectBorder: {
+                width: { unit: 'PT' },
+                color: { color: { rgbColor: {} } },
+                propertyState: 'NOT_RENDERED',
+                dashStyle: 'SOLID',
+              },
+              marginLeft: { magnitude: 9, unit: 'PT' },
+              imageProperties: {
+                contentUri:
+                  'https://lh6.googleusercontent.com/JP5ZiNvFX5lNOTGXft_CafIyGEtGDPE0MIjNFY9EBwad-hD9WzBN5n1aQ54Hrt0gBqLR0uVJW0_sP8N89sxJ42gJuslj7USCOAOhbCule4TGNHwen0HvWpOHuAuy-d5prEODJ9scUqMnoDAuBw',
+                cropProperties: {},
+              },
+              marginBottom: { unit: 'PT', magnitude: 9 },
+            },
+          },
+          objectId: 'kix.wr6s7brrno1m',
+        },
+      };
+
+      cy.wrap(
+        processDocumentContents(
+          elements,
+          listInfo,
+          inlineObjects,
+          imageList,
+          slug,
+          oauthToken
+        )
+      ).then((result) => {
+        let el = result.formattedElements[10];
+
+        // expect(el.type).to.eq('embed');
+        // expect(el.link).to.eq(
+        //   'https://docs.google.com/forms/d/e/1FAIpQLSfVOl8qdT54E2r_T367YsRlka57bUY_fnjedMLezp1Tll9OQw/viewform?usp=send_form'
+        // );
       });
     });
   });
