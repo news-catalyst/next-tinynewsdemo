b27bfa7bdace0d089ee2c266fb6c15f471b7bf63
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-01 16:03:32 -0500

    fetch facebook post embed content from the fb oembed read post api

--- components/nodes/embeds/Facebook.js
+++ components/nodes/embeds/Facebook.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+
+const FB_APP_ID = process.env.NEXT_PUBLIC_FB_APP_ID;
+const FB_CLIENT_TOKEN = process.env.NEXT_PUBLIC_FB_CLIENT_TOKEN;
 
 export default function Facebook({ node }) {
-  return <div />;
+  const [fbPost, setFbPost] = useState(null);
+
+  useEffect(() => {
+    const fbApiUrl =
+      'https://graph.facebook.com/v12.0/oembed_post?url=' + node.link;
+    const fbAccessToken = `${FB_APP_ID}|${FB_CLIENT_TOKEN}`;
+    fetch(fbApiUrl, {
+      headers: {
+        Authorization: `Bearer ${fbAccessToken}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log('result:', result);
+        setFbPost(result.html);
+      });
+  }, [node.link]);
+  return (
+    <div>
+      <script
+        async=""
+        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v12.0"
+      />
+      <div dangerouslySetInnerHTML={{ __html: fbPost }} />
+    </div>
+  );
 }
