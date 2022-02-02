8728439804df88076dbe99a109fae4a1bfacfcad
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-02 15:01:58 -0500

    Inject facebook javascript sdk into the body of the page for fb embeds

--- components/nodes/embeds/Facebook.js
+++ components/nodes/embeds/Facebook.js
@@ -2,11 +2,25 @@ import { useEffect, useState } from 'react';
 
 const FB_APP_ID = process.env.NEXT_PUBLIC_FB_APP_ID;
 const FB_CLIENT_TOKEN = process.env.NEXT_PUBLIC_FB_CLIENT_TOKEN;
+const FB_SDK_URL = 'https://connect.facebook.net/en_US/sdk.js';
 
 export default function Facebook({ node }) {
   const [fbContent, setFbContent] = useState(null);
 
+  function injectScript() {
+    const script = document.createElement('script');
+    script.id = 'load-facebook-sdk';
+    script.src = FB_SDK_URL;
+    script.async = true;
+    console.log('appending', script, 'to the body of the page');
+    const body = document.body;
+    body.appendChild(script);
+  }
+
   useEffect(() => {
+    if (!document.getElementById('load-facebook-sdk')) {
+      injectScript();
+    }
     let fbApiUrl;
     if (/\/videos/.test(node.link)) {
       fbApiUrl =
@@ -20,6 +34,12 @@ export default function Facebook({ node }) {
     }
 
     const fbAccessToken = `${FB_APP_ID}|${FB_CLIENT_TOKEN}`;
+    console.log(
+      'FB tokens (app, client, combo):',
+      FB_APP_ID,
+      FB_CLIENT_TOKEN,
+      fbAccessToken
+    );
     fetch(fbApiUrl, {
       headers: {
         Authorization: `Bearer ${fbAccessToken}`,
@@ -29,16 +49,12 @@ export default function Facebook({ node }) {
     })
       .then((res) => res.json())
       .then((result) => {
-        // console.log('result:', result);
+        console.log('result:', result);
         setFbContent(result.html);
       });
   }, [node.link]);
   return (
     <div>
-      <script
-        async=""
-        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v12.0"
-      />
       <div dangerouslySetInnerHTML={{ __html: fbContent }} />
     </div>
   );
