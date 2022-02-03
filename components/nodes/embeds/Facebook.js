171c79161142e413f0f8cd1f1726df69d69a9a6c
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-03 09:50:01 -0500

    request and store markup for facebook embeds in our database; load fb sdk in embed node

--- components/nodes/embeds/Facebook.js
+++ components/nodes/embeds/Facebook.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
 
-const FB_APP_ID = process.env.NEXT_PUBLIC_FB_APP_ID;
-const FB_CLIENT_TOKEN = process.env.NEXT_PUBLIC_FB_CLIENT_TOKEN;
 const FB_SDK_URL = 'https://connect.facebook.net/en_US/sdk.js';
 
 export default function Facebook({ node }) {
-  const [fbReady, setFbReady] = useState(false);
-  const [fbContent, setFbContent] = useState(null);
-  const [someRando, setSomeRando] = useState(null);
+  const [markup, setMarkup] = useState(null);
+
   async function injectScript() {
     const script = document.createElement('script');
     script.id = 'load-facebook-sdk';
@@ -19,86 +16,43 @@ export default function Facebook({ node }) {
     body.appendChild(script);
   }
 
-  async function initFacebook() {
-    console.log('initializing facebook');
-    window.FB.init({
-      appId: FB_APP_ID,
-      status: true,
-      xfbml: true,
-      version: 'v12.0',
-    });
-  }
-
   const checkFacebookSdk = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       self.timer = window.setTimeout(() => {
         if (window.FB) {
           clearTimeout(self.timer);
           console.log('* found window.FB, clearing timeout and resolving!');
-
-          setFbReady(true);
           resolve(1);
         } else {
           console.log(':( no window.FB, waiting 20ms then retrying', self);
-          setFbReady(false);
           checkFacebookSdk(self);
         }
       }, 20);
     });
   };
 
-  async function loadEmbed(link) {
-    console.log('loadEmbed:', link);
-    let fbApiUrl;
-    if (/\/videos/.test(link)) {
-      fbApiUrl = 'https://graph.facebook.com/v12.0/oembed_video?url=' + link;
-    } else if (/\/posts/.test(node.link)) {
-      fbApiUrl = 'https://graph.facebook.com/v12.0/oembed_post?url=' + link;
-    } else {
-      fbApiUrl = 'https://graph.facebook.com/v12.0/oembed_page?url=' + link;
-    }
-
-    const fbAccessToken = `${FB_APP_ID}|${FB_CLIENT_TOKEN}`;
-
-    const res = await fetch(fbApiUrl, {
-      headers: {
-        Authorization: `Bearer ${fbAccessToken}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'GET',
-    });
-    console.log('result from fetch:', res);
-
-    const data = await res.json();
-    console.log('setting fbContent html:', data.html);
-    setFbContent(data.html);
-  }
-
   useEffect(() => {
-    if (window.FB && fbReady) {
-      async function loadAndParseEmbed() {
-        console.log(
-          'useEffect window.FB found, loading embed for',
-          node.link,
-          '...'
-        );
-        await initFacebook();
-        await loadEmbed(node.link);
-        window.FB.XFBML.parse();
-      }
-      loadAndParseEmbed();
-    } else {
+    async function loadFB() {
       if (!document.getElementById('load-facebook-sdk')) {
-        injectScript();
+        await injectScript();
       }
 
-      checkFacebookSdk();
+      await checkFacebookSdk();
     }
-  }, [node.link, fbReady]);
+
+    loadFB();
+
+    if (node.html) {
+      setMarkup(node.html);
+      console.log('Set markup to the node.html:', node.html);
+    } else {
+      console.error('No markup found for the facebook embed...', node);
+    }
+  }, [node.link]);
 
   return (
     <div>
-      <div dangerouslySetInnerHTML={{ __html: fbContent }} />
+      <div dangerouslySetInnerHTML={{ __html: markup }} />
     </div>
   );
 }
