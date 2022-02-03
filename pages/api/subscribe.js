88111492cab939d0d40725e24a642059383e2476
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-03 17:30:33 -0500

    updated the subscribe api route to use the new letterhead subscribe util function

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,36 +1,12 @@
+import { subscribeLetterhead } from '../../lib/utils';
+
 export default async function Handler(req, res) {
-  const apiUrl = process.env.LETTERHEAD_API_URL;
-  const apiKey = process.env.LETTERHEAD_API_KEY;
-  const channelSlug = process.env.LETTERHEAD_CHANNEL_SLUG;
-  const subscribeApiUrl = apiUrl + 'channels/' + channelSlug + '/subscribers';
   // values from https://github.com/news-catalyst/next-tinynewsdemo/issues/718
-  const channelSubscriberStatusValues = {
-    'Not Subscribed': 0,
-    Subscribed: 1,
-    Unsubscribed: 2,
-    'Needs Verification': 4,
-  };
 
   const { email, name } = JSON.parse(req.body);
 
   try {
-    const response = await fetch(subscribeApiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-        channelSubscriberStatus:
-          channelSubscriberStatusValues['Needs Verification'],
-        email: email,
-        name: name,
-        optin: true,
-      }),
-    });
-
-    const data = await response.json();
-
+    let data = await subscribeLetterhead(email, name);
     return res.status(200).json({ message: data });
   } catch (error) {
     return res.status(500).json({ message: error.message });
