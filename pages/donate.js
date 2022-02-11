6c651a9c600e006a86e9dae2b18ef409f75dabff
Tyler Fisher <tyler@tylerjfisher.com>
2022-02-11 16:36:15 -0500

    Get our image data right for Facebook

--- pages/donate.js
+++ pages/donate.js
@@ -55,6 +55,25 @@ export default function Donate({
     siteMetadata
   );
 
+  let mainImageNode;
+  let mainImage = null;
+  if (page) {
+    try {
+      mainImageNode = localisedPage?.content.find(
+        (node) => node.type === 'mainImage'
+      );
+
+      if (mainImageNode) {
+        mainImage = mainImageNode.children[0];
+        siteMetadata['coverImage'] = mainImage.imageUrl;
+        siteMetadata['coverImageWidth'] = mainImage.width;
+        siteMetadata['coverImageHeight'] = mainImage.height;
+      }
+    } catch (err) {
+      console.error('error finding main image: ', err);
+    }
+  }
+
   return (
     <Layout locale={locale} meta={siteMetadata} page={page} sections={sections}>
       <SectionContainer>
