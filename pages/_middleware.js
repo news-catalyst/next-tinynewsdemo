132eb54b6f96ca289ef2e03b9934b0997d913fd4
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-14 15:14:43 -0500

    _sites/[site] for vercel platforms now with working About page

---
+++ pages/_middleware.js
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+
+export default function middleware(req) {
+  const url = req.nextUrl.clone(); // clone the request url
+  const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
+  const hostname = req.headers.get('host'); // get hostname of request (e.g. demo.vercel.pub)
+  const currentHost = hostname
+    .replace(`.localhost:3000`, '')
+    .replace(`127.0.0.1:3000`, 'oaklyn')
+    .replace(`.vercel.app:3000`, ''); // TBD if we need to change this
+
+  // console.log('middleware host / pathname', currentHost, '/', pathname);
+
+  // Copied this over as commented code in case we want to redirect particular hosts to one central spot
+  // only for demo purposes – remove this if you want to use your root domain as the landing page
+  // if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
+  //   return NextResponse.redirect("https://demo.vercel.pub");
+  // }
+
+  // API routes and the TinyCMS are handled elsewhere tbd
+  if (
+    !pathname.includes('.') &&
+    !pathname.startsWith('/api') &&
+    !pathname.startsWith('/tinycms')
+  ) {
+    url.pathname = `/_sites/${currentHost}${pathname}`;
+    console.log('middleware rewrote url:', url.pathname);
+    return NextResponse.rewrite(url);
+  }
+}
