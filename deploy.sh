f22e62c3ee91147f8fa0ae9b35e7aa87955995dd
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-22 11:05:26 -0500

    updated deploy script after rereading original vercel platforms issue: more conditions to build

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,19 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_ENV: $VERCEL_ENV"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "feature/vercel-platforms" ]] ; then
-  # Proceed with the build
-  echo "🛑 - vercel-platforms feature ignored"
+  echo "🛑 - vercel-platforms feature ignored" # this will be built in a separate Vercel project
   exit 0;
 
+elif [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_ENV" == "preview"  ]] ; then
+  echo "✅ - okay to build main branch in preview env"
+  exit 1;
+
+elif [[ "$VERCEL_GIT_COMMIT_REF" == "production" || "$VERCEL_ENV" == "production"  ]] ; then
+  echo "✅ - okay to build production branch in production env"
+  exit 1;
+  
 else
-  # Proceed with the build
-  echo "✅ - build can proceed"
+  # Skip the build
+  echo "🛑 - otherwise skipping the build in $VERCEL_GIT_COMMIT_REF and $VERCEL_ENV"
   exit 1;
 fi
