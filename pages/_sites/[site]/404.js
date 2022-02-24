f59f636137b8ea897f5a1d53ba5d08f49267da8e
Jacqui Lough <lough.jacqui@gmail.com>
2022-02-24 14:44:03 -0500

    debug: removed i18n for now

--- pages/_sites/[site]/404.js
+++ pages/_sites/[site]/404.js
@@ -91,6 +91,11 @@ export async function getStaticProps({ locale, params }) {
     }
   }
 
+  // why? Error: Error serializing `.locale` returned from `getStaticProps` in "/_sites/[site]/tags/[slug]".
+  // Reason: `undefined` cannot be serialized as JSON. Please use `null` or omit this value.
+  if (!locale) {
+    locale = null;
+  }
   return {
     props: {
       locale,
