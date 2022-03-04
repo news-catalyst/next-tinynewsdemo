88a37ad1b4d68f0f5cc7bba61520ca46efd296f4
Jacqui Lough <lough.jacqui@gmail.com>
2022-03-04 15:08:12 -0500

    vercel platform update for the newsletter subscribe api endpoint

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,8 +1,30 @@
+import { getOrgSettings } from '../../lib/articles';
+import { findSetting } from '../../lib/utils.js';
+
 export default async function Handler(req, res) {
-  const apiUrl = process.env.LETTERHEAD_API_URL;
-  const apiKey = process.env.LETTERHEAD_API_KEY;
-  const channelSlug = process.env.LETTERHEAD_CHANNEL_SLUG;
-  const subscribeApiUrl = apiUrl + 'channels/' + channelSlug + '/subscribers';
+  const apiUrl = process.env.HASURA_API_URL;
+  const site = req.query.site;
+
+  const settingsResult = await getOrgSettings({
+    url: apiUrl,
+    site: site,
+  });
+
+  if (settingsResult.errors) {
+    console.error('Subscribe API - settings error:', settingsResult.errors);
+    throw settingsResult.errors;
+  }
+
+  const settings = settingsResult.data.settings;
+  const letterheadApiUrl = findSetting(settings, 'LETTERHEAD_API_URL');
+  const letterheadApiKey = findSetting(settings, 'LETTERHEAD_API_KEY');
+  const letterheadChannelSlug = findSetting(
+    settings,
+    'LETTERHEAD_CHANNEL_SLUG'
+  );
+  const subscribeApiUrl =
+    letterheadApiUrl + 'channels/' + letterheadChannelSlug + '/subscribers';
+
   // values from https://github.com/news-catalyst/next-tinynewsdemo/issues/718
   const channelSubscriberStatusValues = {
     'Not Subscribed': 0,
@@ -18,7 +40,7 @@ export default async function Handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${apiKey}`,
+        Authorization: `Bearer ${letterheadApiKey}`,
       },
       body: JSON.stringify({
         channelSubscriberStatus:
