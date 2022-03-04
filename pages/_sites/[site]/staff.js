d865b6f806410529d68ace171ebafbd6ffbf3ce5
Jacqui Lough <lough.jacqui@gmail.com>
2022-03-04 10:41:54 -0500

    bugfix: staff page no longer 404s, this page is no longer tied to google doc

--- pages/_sites/[site]/staff.js
+++ pages/_sites/[site]/staff.js
@@ -50,12 +50,6 @@ export async function getStaticProps({ params }) {
     };
     // throw errors;
   } else {
-    if (!data.page_slug_versions || !data.page_slug_versions[0]) {
-      return {
-        notFound: true,
-      };
-    }
-    page = data.page_slug_versions[0].page;
     sections = data.categories;
     authors = data.authors;
     siteMetadata = data.site_metadatas[0].site_metadata_translations[0].data;
