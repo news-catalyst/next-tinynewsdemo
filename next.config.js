0dc2fce681dfcf986f3be53309698aec4dcbec3e
Jacqui Lough <lough.jacqui@gmail.com>
2022-03-17 11:37:42 +1100

    replaced hardcoded assets.tinynewsco.org with NEXT_PUBLIC_ASSETS_DOMAIN env var

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 require('dotenv').config({ path: '.env.local' });
 
+// to-do: replace d32qj1gowzeibr.cloudfront.net with assets.tinynewsco.dev
 module.exports = {
   images: {
     domains: [
       'tnc-test-upload-bucket.s3.amazonaws.com',
       'tnc-staging-upload-bucket.s3.amazonaws.com',
       'assets.tinynewsco.org',
+      'd32qj1gowzeibr.cloudfront.net',
       'tiny-news-demo-assets-dev.s3.amazonaws.com',
       'wherebyspace.nyc3.digitaloceanspaces.com',
       'tiny-news-demo-assets-oaklyn.s3.amazonaws.com',
