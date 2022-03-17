bc8c4d1bc25c5669cdeafddd3cdd0fae4dba0bc6
Jacqui Lough <lough.jacqui@gmail.com>
2022-03-17 11:37:42 +1100

    replaced hardcoded assets.tinynewsco.org with NEXT_PUBLIC_ASSETS_DOMAIN env var

--- cypress/integration/document_parser_spec.js
+++ cypress/integration/document_parser_spec.js
@@ -1650,8 +1650,7 @@ describe('document parser', () => {
       ).as('googleForms');
 
       cy.stub(TinyS3, 'upload').returns({
-        s3Url:
-          'https://assets.tinynewsco.org/oaklyn-test/category-slug/article-slug/imagekix123.png',
+        s3Url: `https://${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/oaklyn-test/category-slug/article-slug/imagekix123.png`,
         height: 640,
         width: 480,
       });
@@ -2313,8 +2312,7 @@ describe('document parser', () => {
       ];
       listInfo = { 'kix.qqyukljg8mnr': 'BULLET' };
       imageList = {
-        'kix.wr6s7brrno1m':
-          'https://assets.tinynewsco.org/oaklyn/cypress-test-doc/imagekix.wr6s7brrno1m.png',
+        'kix.wr6s7brrno1m': `https://${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/oaklyn-test/category-slug/article-slug/imagekix.wr6s7brrno1m.png`,
       };
       inlineObjects = {
         'kix.wr6s7brrno1m': {
@@ -2361,7 +2359,7 @@ describe('document parser', () => {
         expect(el.children[0].height).to.eq(640);
         expect(el.children[0].width).to.eq(480);
         expect(el.children[0].imageUrl).to.eq(
-          'https://assets.tinynewsco.org/oaklyn-test/category-slug/article-slug/imagekix123.png'
+          `https://${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/oaklyn-test/category-slug/article-slug/imagekix123.png`
         );
       });
     });
