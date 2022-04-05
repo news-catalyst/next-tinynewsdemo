4f501aa2ef9086d54ef1c28797e4b82003c90df1
Jacqui Lough <lough.jacqui@gmail.com>
2022-04-05 14:13:01 +1000

    initial (not working) version of revalidate api endpoint

---
+++ pages/api/revalidate.js
@@ -0,0 +1,42 @@
+import { findSetting, getOrgSettings } from '../../lib/settings';
+
+export default async function handler(req, res) {
+  const apiUrl = process.env.HASURA_API_URL;
+  const site = req.query.site;
+
+  const settingsResult = await getOrgSettings({
+    url: apiUrl,
+    site: site,
+  });
+
+  if (settingsResult.errors) {
+    console.error('DocAPI id Settings error:', settingsResult.errors);
+    throw settingsResult.errors;
+  }
+
+  const settings = settingsResult.data.settings;
+  const apiToken = findSetting(settings, 'API_TOKEN');
+
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== apiToken) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  const pathToRevalidate = req.query.path;
+  if (!pathToRevalidate) {
+    return res
+      .status(401)
+      .json({ message: 'Missing required ?path=/to/revalidate query param' });
+  }
+
+  try {
+    // await res.unstable_revalidate(`${pathToRevalidate}?site=${site}`);
+    await res.unstable_revalidate(pathToRevalidate);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    console.error(err);
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
