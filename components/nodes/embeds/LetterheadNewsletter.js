94f322d073c545de7678b55b3ba0d6aa8de110b7
Jacqui Lough <lough.jacqui@gmail.com>
2022-04-19 08:50:31 +1000

    Include published date in newsletter index and edition pages, sort index by published date

--- components/nodes/embeds/LetterheadNewsletter.js
+++ components/nodes/embeds/LetterheadNewsletter.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import tw, { styled } from 'twin.macro';
 
-const NewsletterContainer = tw.div`flex mt-1 pt-8 mb-12 w-full mx-auto`;
+const NewsletterContainer = tw.div`flex mb-12 w-full mx-auto`;
 const NewsletterMarkupContainer = tw.div`flex w-full mx-auto`;
 
 export default function LetterheadNewsletter({ content }) {
