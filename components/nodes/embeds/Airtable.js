7270278b1dd36a6277ccabacd0d0c0c630e2ecf2
Pilar de Haro <pilardeharo56@gmail.com>
2022-04-25 08:25:33 -0700

    Add airtable embed

---
+++ components/nodes/embeds/Airtable.js
@@ -0,0 +1,21 @@
+export default function Airtable({ node, amp }) {
+  //this is to add the 'embed' before the id since that's how airtable set it up
+  const partial_src = node.slice(0, 21);
+  const id = node.slice(21);
+
+  const el = amp ? (
+    <div />
+  ) : (
+    <iframe
+      src={partial_src + 'embed/' + id}
+      width="100%"
+      height="553"
+      frameBorder="0"
+      marginHeight="0"
+      onmousewheel=""
+      marginWidth="0"
+    />
+  );
+
+  return el;
+}
