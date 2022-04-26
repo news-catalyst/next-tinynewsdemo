459bffc6579127ece99a13817b0d27770b7cce08
Tyler Fisher <tyler@tylerjfisher.com>
2022-04-26 10:41:16 -0400

    Don't rely on slice for partial src, just hardcode it

--- components/nodes/embeds/Airtable.js
+++ components/nodes/embeds/Airtable.js
@@ -1,13 +1,12 @@
 export default function Airtable({ node, amp }) {
   //this is to add the 'embed' before the id since that's how airtable set it up
-  const partial_src = node.link.slice(0, 21);
   const id = node.link.split('/').slice(3);
 
   const el = amp ? (
     <div />
   ) : (
     <iframe
-      src={partial_src + 'embed/' + id}
+      src={'https://airtable.com/embed/' + id}
       width="100%"
       height="553"
       frameBorder="0"
