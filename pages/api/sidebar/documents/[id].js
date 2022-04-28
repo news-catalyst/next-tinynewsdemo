61bd187fcf66a3601fd65f1590f2456cb72c7e95
Jacqui Lough <lough.jacqui@gmail.com>
2022-04-28 10:43:27 +1000

    cypress: more debugging and pass the slug param in insertOrganization (doh)

--- pages/api/sidebar/documents/[id].js
+++ pages/api/sidebar/documents/[id].js
@@ -34,9 +34,9 @@ export default async function Handler(req, res) {
   // Check the API token
   if (req.query.token !== apiToken || !req.query.id) {
     console.error(
-      `Supplied token '${req.query.token}' doesn't match '${apiToken}'`
+      `Supplied token '${req.query.token}' doesn't match '${apiToken}'`,
+      settings
     );
-    console.error('settings:', settings);
     return res.status(401).json({ message: 'Invalid API token' });
   }
 
