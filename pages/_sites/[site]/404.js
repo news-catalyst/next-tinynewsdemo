0b2fd652a268674c8c50e609eac7ef30c893fd48
Jacqui Lough <lough.jacqui@gmail.com>
2022-04-29 08:02:09 +1000

    bugfix: added same check for section title to 404 as article and newsletter pages

--- pages/_sites/[site]/404.js
+++ pages/_sites/[site]/404.js
@@ -75,7 +75,14 @@ export async function getStaticProps({ params }) {
   } else {
     sections = data.categories;
     for (var i = 0; i < sections.length; i++) {
-      sections[i]?.title = sections[i]?.category_translations[0]?.title;
+      if (
+        sections[i] &&
+        sections[i].category_translations &&
+        sections[i].category_translations[0] &&
+        sections[i].category_translations[0].title
+      ) {
+        sections[i].title = sections[i].category_translations[0].title;
+      }
     }
     try {
       siteMetadata = data.site_metadatas[0].site_metadata_translations[0].data;
