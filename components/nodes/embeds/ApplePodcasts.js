70844aa489b32388e95bc1216adba27109538632
Pilar de Haro <pilardeharo56@gmail.com>
2022-05-11 13:40:42 -0700

    adding Spotify and Apple Podcast embeds

--- components/nodes/embeds/ApplePodcasts.js
+++ components/nodes/embeds/ApplePodcasts.js
@@ -1,14 +1,18 @@
 export default function ApplePodcasts({ node, amp }) {
+  const embedUrl = node.link.split('//')[1];
+  console.log(embedUrl);
+
   const el = amp ? (
     <div />
   ) : (
     <iframe
       allow="autoplay *; encrypted-media *; fullscreen *"
       frameBorder="0"
-      height="175"
+      height="450"
       sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-      src={node.link}
+      src={`https://embed.${embedUrl}`}
     />
   );
+
   return el;
 }
