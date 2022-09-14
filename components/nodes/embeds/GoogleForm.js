91fcafa818ebeb29b8b4a173b821380e585dd443
Pilar de Haro <100712902+pdeharo333@users.noreply.github.com>
2022-09-14 13:10:24 -0700

    GoogleForm Fix

--- components/nodes/embeds/GoogleForm.js
+++ components/nodes/embeds/GoogleForm.js
@@ -1,9 +1,16 @@
 export default function GoogleForm({ node, amp }) {
+  const id = node.link.split('/')[6];
+
+  //check that node and node.link arent falsy/undefined log error and don't return anything
+  //check node/link.split also
+
   const el = amp ? (
     <div />
   ) : (
     <iframe
-      src={node.link}
+      src={
+        'https://docs.google.com/forms/d/e/' + id + '/viewform?embedded=true'
+      }
       width="100%"
       height="640px"
       frameBorder="0"
@@ -11,5 +18,10 @@ export default function GoogleForm({ node, amp }) {
       marginWidth="0"
     />
   );
-  return el;
+
+  if (!node.link || !node || !node.link.split('/')) {
+    console.log('The node.link or node is empty');
+  } else {
+    return el;
+  }
 }
