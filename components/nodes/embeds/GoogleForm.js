07524c4865310f13fa96172f133a4868bf3c4412
Pilar de Haro <100712902+pdeharo333@users.noreply.github.com>
2022-09-14 13:18:28 -0700

    added iframe changes for embed fix

--- components/nodes/embeds/GoogleForm.js
+++ components/nodes/embeds/GoogleForm.js
@@ -1,9 +1,6 @@
 export default function GoogleForm({ node, amp }) {
   const id = node.link.split('/')[6];
 
-  //check that node and node.link arent falsy/undefined log error and don't return anything
-  //check node/link.split also
-
   const el = amp ? (
     <div />
   ) : (
@@ -18,6 +15,9 @@ export default function GoogleForm({ node, amp }) {
       marginWidth="0"
     />
   );
+
+  //check that node and node.link arent falsy/undefined log error and don't return anything
+  //check node/link.split also
 
   if (!node.link || !node || !node.link.split('/')) {
     console.log('The node.link or node is empty');
