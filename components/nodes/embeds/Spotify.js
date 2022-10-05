bd619e0b5810a343a0806fed21b4c8aaa5f4dd83
Chelsea J <108034069+chelsea-tnc@users.noreply.github.com>
2022-10-05 16:20:07 -0700

    Fix Spotify embeds (#1296)

--- components/nodes/embeds/Spotify.js
+++ components/nodes/embeds/Spotify.js
@@ -1,10 +1,12 @@
 export default function Spotify({ node, amp }) {
-  const id = node.link.split('/').slice(-1);
+  const subNodes = node.link.split('/');
+  const id = subNodes.slice(-1);
+  const epTrack = subNodes[3];
   const el = amp ? (
     <div />
   ) : (
     <iframe
-      src={'https://open.spotify.com/embed/track/' + id}
+      src={'https://open.spotify.com/embed/' + epTrack + '/' + id}
       width="300"
       height="380"
       frameBorder="0"
